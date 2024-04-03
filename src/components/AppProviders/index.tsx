e41529161e4f2aa4cddd82832d7f64921e7aad8a
Cristian Romero <cristianrom66@gmail.com>
2024-04-03 02:50:20 -0500

    FIX: forcing theme to darkmode regardless the os theme setting

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -3,15 +3,15 @@ import { ThemeProvider } from '@mui/material'
 import { CacheProvider } from '@emotion/react'
 import createEmotionCache from '@/utils/createEmotionCache'
 import Web3ModalProvider from '@/services/Web3ModalProvider'
-import initTheme from '@/styles/theme'
+import { useThemeModes } from '@/hooks/useThemeModes'
 
 const AppProviders = ({ children }: { children: ReactNode | ReactNode[] }) => {
-  // const theme = useThemeModes() //! will be discommented until we define a proper squema structure
+  const theme = useThemeModes() //! will be discommented until we define a proper squema structure
   const clientSideEmotionCache = createEmotionCache()
 
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <ThemeProvider theme={initTheme(true)}>
+      <ThemeProvider theme={theme}>
         <Web3ModalProvider>{children}</Web3ModalProvider>
       </ThemeProvider>
     </CacheProvider>
