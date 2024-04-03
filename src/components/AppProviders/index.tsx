fe55900ec69e08c257057f99bba8ac460ce676f1
Cristian Romero <cristianrom66@gmail.com>
2024-04-03 02:11:47 -0500

    init dark theme fixed. WIP: Abstracting all vertex data from API to redux to be able to fetch price data

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -1,17 +1,17 @@
 import type { ReactNode } from 'react'
 import { ThemeProvider } from '@mui/material'
 import { CacheProvider } from '@emotion/react'
-import { useThemeModes } from '@/hooks/useThemeModes'
 import createEmotionCache from '@/utils/createEmotionCache'
 import Web3ModalProvider from '@/services/Web3ModalProvider'
+import initTheme from '@/styles/theme'
 
 const AppProviders = ({ children }: { children: ReactNode | ReactNode[] }) => {
-  const theme = useThemeModes()
+  // const theme = useThemeModes() //! will be discommented until we define a proper squema structure
   const clientSideEmotionCache = createEmotionCache()
 
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={initTheme(true)}>
         <Web3ModalProvider>{children}</Web3ModalProvider>
       </ThemeProvider>
     </CacheProvider>
