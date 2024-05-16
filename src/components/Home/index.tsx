ebde69ad137768661a0a6469355366dd4e654d65
Marcos <n4140379@gmail.com>
2024-05-15 21:18:59 -0400

    CHANGE: replaced old home with new clean file

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,61 +1,11 @@
-import { Box, Grid } from '@mui/material'
+import React from 'react'
 
-import CollateralsTable from '@/components/CollateralsTable'
-import OwnBalances from '@/components/OwnBalances'
-import PriceChart from '@/components/PriceChart'
-import Sidebar from '@/components/Sidebar'
-import TxInteraction from '@/components/TxInteraction'
-import AssetsHeader from '@/components/common/AssetsHeader'
-import Header from '@/components/common/Header'
-
-import css from './styles.module.css'
-
-const Home = () => (
-  <>
-    <header className={css.headerContainer}>
-      <Box className={css.headerContent}>
-        <Box className={css.header}>
-          <Header />
-        </Box>
-      </Box>
-    </header>
-
-    <div>
-      <aside className={css.sidebar}>
-        <Sidebar />
-      </aside>
-    </div>
-
-    <Box className={css.main}>
-      <div className={css.content}>
-        <AssetsHeader />
-
-        <Box className={css.cards}>
-          <Grid container spacing={2} className={css.cardsContainer}>
-            <Grid item xs={9}>
-              <Box className={css.card}>
-                <PriceChart />
-              </Box>
-
-              <Box className={css.card}>
-                <CollateralsTable />
-              </Box>
-            </Grid>
-
-            <Grid item xs={3}>
-              <Box className={css.card}>
-                <TxInteraction />
-              </Box>
-
-              <Box className={css.card}>
-                <OwnBalances />
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </div>
-    </Box>
-  </>
-)
+const Home = () => {
+  return (
+    <>
+      <p>New Home</p>
+    </>
+  )
+}
 
 export default Home
