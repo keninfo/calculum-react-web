5481a0ac75a0c57bb3c89e1967baf986b2e95dba
Marcos <n4140379@gmail.com>
2024-05-17 20:20:13 -0400

    ADDED: tailwind and mui/base in favor of mui/ui, also added fontawesome

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    colors: {
+      darkness: '#111111',
+      smoke: '#13191C',
+      carmesi: '#9C061F',
+    },
+  },
+  plugins: [],
+}
