f882db06e32c829c754399560d38e77af5c910b4
Marcos <n4140379@gmail.com>
2024-05-18 11:53:24 -0400

    FIXED: 404 page height

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ import { usePathname } from 'next/navigation'
 const Index = () => {
   const pathname = usePathname()
   return (
-    <div>
+    <div className="h-screen">
       <h1 className="text-red-500">{pathname.toUpperCase().slice(1)}</h1>
       <p>Page doesn`t exist yet</p>
     </div>
