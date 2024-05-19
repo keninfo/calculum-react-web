b125c4a02ddbf50f90e4c17c0674940dcf031dfe
Marcos <n4140379@gmail.com>
2024-05-19 01:18:40 -0400

    ADDED: sidebar context provider

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -11,19 +11,32 @@ interface CoinContextType {
   setCoin: React.Dispatch<React.SetStateAction<string>>
 }
 
+interface SidebarContextType {
+  isSidebarOpen: boolean
+  setSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
 export const CoinContext = createContext<CoinContextType>({
   coin: 'ADA',
   setCoin: () => {},
+})
+
+export const SidebarContext = createContext<SidebarContextType>({
+  isSidebarOpen: true,
+  setSidebarOpen: () => {},
 })
 const clientSideEmotionCache = createEmotionCache()
 
 const AppProviders = ({ children }: { children: ReactNode | ReactNode[] }) => {
   const [coin, setCoin] = useState<string>('ADA')
+  const [isSidebarOpen, setSidebarOpen] = useState<boolean>(true)
 
   return (
     <CacheProvider value={clientSideEmotionCache}>
       <CoinContext.Provider value={{ coin, setCoin }}>
-        <Web3ModalProvider>{children}</Web3ModalProvider>
+        <SidebarContext.Provider value={{ isSidebarOpen, setSidebarOpen }}>
+          <Web3ModalProvider>{children}</Web3ModalProvider>
+        </SidebarContext.Provider>
       </CoinContext.Provider>
     </CacheProvider>
   )
