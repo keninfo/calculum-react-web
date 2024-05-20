b5a2c337763789108cbbceb0fcc9da42e6bf7d6a
Marcos <n4140379@gmail.com>
2024-05-19 23:06:31 -0400

    FIXED: sidebar responsiveness, separated into easier to read components

---
+++ src/components/Sidebar/SidebarGroup.tsx
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react'
+
+import { SidebarContext } from '@/components/AppProviders'
+
+import SidebarGroupItem from './SidebarGroupItem'
+import type { NavigationGroup } from './config'
+
+const SidebarGroup = (group: NavigationGroup) => {
+  const { isSidebarOpen } = useContext(SidebarContext)
+  return (
+    <div key={group.name} className="w-full mb-[5vh]">
+      {isSidebarOpen && <p className="text-[1.5vh]">{group.name}</p>}
+      {group.list.map(SidebarGroupItem)}
+    </div>
+  )
+}
+
+export default SidebarGroup
