3349deedeb700bf9043e6fbe20cdaf33b8a4cd85
Marcos <n4140379@gmail.com>
2024-05-19 23:10:28 -0400

    FIXED: coin select styling

--- src/components/ChartOptions/CoinSelect/Index.tsx
+++ src/components/ChartOptions/CoinSelect/Index.tsx
@@ -1,13 +1,13 @@
-import { useContext } from 'react'
+import React, { useContext } from 'react'
 
 import { CoinContext } from '@/components/AppProviders'
 
-const Index = ({ coins }: { coins: string[] }) => {
+const CoinSelect = ({ coins }: { coins: string[] }) => {
   const { coin, setCoin } = useContext(CoinContext)
 
   return (
     <select
-      className="px-10 py-4 h-fit w-fit rounded-lg text-xs border-white text-[#201F31] text-center"
+      className="px-10 py-2 h-fit w-fit rounded-2xl text-md border border-white bg-smoke text-white text-left"
       id="cryptoCoin"
       onChange={(e) => setCoin(e.target.value)}
       value={coin}
@@ -21,4 +21,4 @@ const Index = ({ coins }: { coins: string[] }) => {
   )
 }
 
-export default Index
+export default CoinSelect
