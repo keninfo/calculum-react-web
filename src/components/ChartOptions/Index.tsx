2a1dcdf9a8c2fa14d35c5be8aa2fa48856170c30
Marcos <n4140379@gmail.com>
2024-05-20 09:08:36 -0400

    HOTFIX: fixed import in chart options preventing build

--- src/components/ChartOptions/Index.tsx
+++ src/components/ChartOptions/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import CoinSelect from './CoinSelect/Index'
+import CoinSelect from './CoinSelect'
 import DatePicker from './DatePicker/Index'
 import SetDays from './SetDays/Index'
 
