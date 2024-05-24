ab7fcf2fa9b3c9f973e10464fbccba21555303a6
Marcos <n4140379@gmail.com>
2024-05-24 11:29:58 -0400

    FIXED: connect button to allow disconnect

--- src/components/common/ConnectButton/index.tsx
+++ src/components/common/ConnectButton/index.tsx
@@ -3,16 +3,15 @@ import { useWeb3Modal } from '@web3modal/wagmi/react'
 import { useAccount, useDisconnect } from 'wagmi'
 
 import ClearButton from '@/components/common/ClearButton'
-import { shortenAddress } from '@/utils/formatters'
 
 const ConnectButton = () => {
   const { open } = useWeb3Modal()
-  const { address, isConnected } = useAccount()
+  const { isConnected } = useAccount()
   const { disconnect } = useDisconnect()
 
   return (
     <ClearButton handleClickClearButton={isConnected ? () => disconnect() : () => open()}>
-      {isConnected ? <p>{shortenAddress(address)}</p> : <p>Connect Wallet</p>}
+      {isConnected ? <p>Disconnect Wallet</p> : <p>Connect Wallet</p>}
     </ClearButton>
   )
 }
