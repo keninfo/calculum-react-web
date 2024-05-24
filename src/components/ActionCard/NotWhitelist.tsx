4397b8d9e1b14c02b54586b1da939b97701543f0
Marcos <n4140379@gmail.com>
2024-05-24 11:24:53 -0400

    ADDED: not in whitelist message component

---
+++ src/components/ActionCard/NotWhitelist.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import ConnectButton from '@/components/common/ConnectButton'
+
+const NotWhitelist = () => {
+  return (
+    <>
+      <p className="text-center">
+        {`Hey there! It seems you're not on our whitelist. Please reach out to us so we can get you properly authorized
+        and you can start using Bear Protocol.`}
+        <br></br>
+        {`We're here to help!`}
+      </p>
+      <p className="text-center my-4 text-carmesi text-xl">bearprotocol@gmail.com</p>
+      <ConnectButton />
+    </>
+  )
+}
+
+export default NotWhitelist
