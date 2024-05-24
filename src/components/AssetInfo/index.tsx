ba1ea697c2d5d501705c68b08aee56ac5c88ccf0
Marcos <n4140379@gmail.com>
2024-05-24 11:29:40 -0400

    FIXED: info for asset

--- src/components/AssetInfo/index.tsx
+++ src/components/AssetInfo/index.tsx
@@ -14,26 +14,20 @@ const AssetInfo = () => {
           <h4 className="text-3xl mb-4">{coin.toUpperCase()}</h4>
           <CryptoIcon coin={coin} className="size-[2rem]" />
         </div>
-        <h5>TVL & Traders</h5>
-        <p> Lorem ipsum</p>
-        <h5>Product Description</h5>
+        <h5>Volatility Targeting</h5>
         <p>
           {' '}
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos porro veniam maiores soluta quam.
-          Corporis animi expedita repellendus molestias ratione, possimus nobis iste dolore eligendi veritatis ad
-          assumenda earum! Debitis
+          Volatility targeting adjusts asset allocation to maintain a consistent portfolio risk level. It involves
+          changing exposure based on market volatility to meet a set volatility target. This strategy aims to stabilize
+          returns and manage risk effectively.
         </p>
-        <h5>Risk</h5>
+        <h5>Disclaimers</h5>
         <p>
           {' '}
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos porro veniam maiores soluta quam.
           Corporis animi expedita repellendus molestias ratione, possimus nobis iste dolore eligendi veritatis ad
           assumenda earum! Debitis.
         </p>
-        <div className="flex start items-center">
-          <h5 className="text-carmesi">Orders made in the past</h5>
-          <p className="text-3xl ml-4">0</p>
-        </div>
       </div>
     </Card>
   )
