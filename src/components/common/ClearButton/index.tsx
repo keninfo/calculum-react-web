9d13e60379a6e26e4480e954c9f2408c8d240d30
Marcos <n4140379@gmail.com>
2024-05-24 11:29:24 -0400

    ADDED: disabled feature to clear button

--- src/components/common/ClearButton/index.tsx
+++ src/components/common/ClearButton/index.tsx
@@ -5,12 +5,14 @@ import { Button } from '@mui/base'
 type ClearButtonProps = {
   handleClickClearButton: () => void
   children: ReactNode
+  disabled?: boolean
 }
 
-const ClearButton = ({ handleClickClearButton, children }: ClearButtonProps) => (
+const ClearButton = ({ handleClickClearButton, children, disabled }: ClearButtonProps) => (
   <Button
     onClick={handleClickClearButton}
     className="bg-smoke py-6 flex justify-center rounded-xl text-white  hover:scale-110 w-[100%] hover:text-carmesi"
+    disabled={disabled}
   >
     {children}
   </Button>
