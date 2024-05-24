e79a0e8dafa0ab0f31e017d64748c36c93df733b
Daniela <n4140379@gmail.com>
2024-05-24 11:48:33 -0400

    REMOVED: open sidebar option, for the moment

--- src/components/Sidebar/SidebarFooter.tsx
+++ src/components/Sidebar/SidebarFooter.tsx
@@ -1,6 +1,5 @@
-import type { IconName } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+// import type { IconName } from '@fortawesome/fontawesome-svg-core'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { useContext } from 'react'
 
 import { SidebarContext } from '@/components/AppProviders'
@@ -8,19 +7,19 @@ import ConnectButton from '@/components/common/ConnectButton'
 import SmallConnectButton from '@/components/common/ConnectButton/small'
 
 const SidebarFooter = () => {
-  const { isSidebarOpen, setSidebarOpen } = useContext(SidebarContext)
+  const { isSidebarOpen } = useContext(SidebarContext)
 
-  const changeSidebar = () => {
-    setSidebarOpen((prevstate) => !prevstate)
-  }
+  // const changeSidebar = () => {
+  //   setSidebarOpen((prevstate) => !prevstate)
+  // }
 
   return (
     <div className="w-[70%] mx-auto">
-      {!isSidebarOpen && (
+      {/* {!isSidebarOpen && (
         <button className="w-full z-100 rounded-lg mb-[5vh] text-2xl text-carmesi" onClick={changeSidebar}>
           <FontAwesomeIcon icon={['fas', 'angles-right' as IconName]} />
         </button>
-      )}
+      )} */}
       {!isSidebarOpen && <SmallConnectButton />}
       {isSidebarOpen && <ConnectButton />}
     </div>
