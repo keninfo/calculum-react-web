af572418dd955da2ea387f8e60aec2b6964d5efe
Carl Gauss <carlgauss221@gmail.com>
2024-05-28 00:19:39 -0400

    FIXED: claim message background

--- src/components/ActionCard/Claim.tsx
+++ src/components/ActionCard/Claim.tsx
@@ -66,8 +66,8 @@ const Claim = () => {
       {userWithdrawalsStatusResult == 4 ||
         (userWithdrawalsStatusResult == 5 && (
           <>
-            <p className="mt-[4vh] mb-[2vh]">
-              Your assets are still pending for Withdraw, wait one Epoch to be able to claim them!
+            <p className="bg-carmesi  px-[2vw] py-[1vh] rounded-lg mt-[4vh] mb-[2vh]">
+              Your assets are still pending for Withdraw/Redeem, wait one Epoch to be able to claim them!
             </p>
             <div className="text-center border-2 bg-smoke rounded-lg px-[2vw] py-[1vh]  border-white">
               <h4>PENDING ASSETS</h4>
