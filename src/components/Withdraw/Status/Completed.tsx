6cd125ab60068da53f2947161c8dc06e2be71fc7
Carl Gauss <carlgauss221@gmail.com>
2024-05-29 11:18:20 -0400

    REFACTOR: moved select withdraw or redeem to parent

--- src/components/Withdraw/Status/Completed.tsx
+++ src/components/Withdraw/Status/Completed.tsx
@@ -1,40 +1,11 @@
-import React, { useState } from 'react'
-
-import { useAccount } from 'wagmi'
-
-import ContractReads from '@/hooks/useContractReads'
-import { formatShares } from '@/utils/formatters'
+import React from 'react'
 
 import Redeem from '../Redeem'
 import WithdrawAsset from '../WithdrawAsset'
 
-const Completed = () => {
-  const [selected, setSelected] = useState<number>(0)
-  const { address } = useAccount()
-  const { SymbolShares, BalanceAsset } = ContractReads()
-
-  const BalanceAssetResult = BalanceAsset(address).data as bigint
-
+const Completed = ({ selected }: { selected: number }) => {
   return (
     <>
-      <p className="text-center text-xs mt-[2vh]">
-        You have {parseFloat(formatShares(BalanceAssetResult))}
-        <b className="text-carmesi"> {SymbolShares().data as string}</b> in Wallet
-      </p>
-      <div className="flex justify-between p-[1vw] mb-[2vh] text-sm">
-        <div
-          className={`text-center border-2  bg-smoke rounded-lg px-[2vw] py-[1vh] cursor-pointer  hover:scale-105 ${selected == 0 ? 'border-white' : 'border-smoke'}`}
-          onClick={() => setSelected(0)}
-        >
-          <h4>Withdraw</h4>
-        </div>
-        <div
-          className={`text-center border-2  bg-smoke rounded-lg px-[2vw] py-[1vh] cursor-pointer  hover:scale-105 ${selected == 1 ? 'border-white' : 'border-smoke'}`}
-          onClick={() => setSelected(1)}
-        >
-          <h4>Redeem</h4>
-        </div>
-      </div>
       <div className="my-[2vh]">
         {selected == 0 && <WithdrawAsset />}
         {selected == 1 && <Redeem />}
