6974732b3dfefb4941278a2e6b77082b68a51987
Carl Gauss <carlgauss221@gmail.com>
2024-05-29 11:15:45 -0400

    ADDED: approve hook

---
+++ src/hooks/useApprove.ts
@@ -0,0 +1,24 @@
+import type { Hash } from 'viem'
+import { parseUnits } from 'viem'
+
+import { useWriteContract } from 'wagmi'
+
+import { calculumVaultContract } from '@/contracts/calculumVault'
+import { usdcContract } from '@/contracts/usdc'
+
+const useApprove = () => {
+  const { writeContract } = useWriteContract()
+
+  const ApproveAssets = (amount: number) => {
+    writeContract({
+      abi: usdcContract.abi,
+      address: usdcContract.address as Hash,
+      functionName: 'approve',
+      args: [calculumVaultContract.address, parseUnits(amount.toString(), 6)],
+    })
+  }
+
+  return { ApproveAssets }
+}
+
+export default useApprove
