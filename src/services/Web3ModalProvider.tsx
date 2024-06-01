0be9c90276bc3b8accff672101ad8ac6f5ec5489
Carl Gauss <carlgauss221@gmail.com>
2024-06-01 15:35:04 -0400

    FIXED: added mainnet to provider config

--- src/services/Web3ModalProvider.tsx
+++ src/services/Web3ModalProvider.tsx
@@ -9,7 +9,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { cookieStorage, createStorage } from 'wagmi'
 import { type State, WagmiProvider } from 'wagmi'
-import { arbitrumSepolia } from 'wagmi/chains'
+import { arbitrumSepolia, mainnet } from 'wagmi/chains'
 import { metaMask } from 'wagmi/connectors'
 
 import { WALLET_CONNECT_PROJECT_ID } from '@/utils/constants'
@@ -25,7 +25,7 @@ const metadata = {
   icons: ['https://avatars.githubusercontent.com/u/37784886'],
 }
 
-const chains = [arbitrumSepolia] as const
+const chains = [arbitrumSepolia, mainnet] as const
 export const config = defaultWagmiConfig({
   chains,
   projectId,
