4fdca69d41a21044521d845128aab176a2a8efc9
Carl Gauss <carlgauss221@gmail.com>
2024-06-04 13:31:05 -0400

    REFACTOR: changed home to use grid

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import type { SetStateAction } from 'react'
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import ActionCard from '@/components/ActionCard'
-import { CoinContext } from '@/components/AppProviders'
-import AssetInfo from '@/components/AssetInfo'
-import CoinSelect from '@/components/ChartOptions/CoinSelect'
-import Products from '@/components/CollateralsTable'
-import CryptoIcon from '@/components/common/CryptoIcon'
+import Chart from '@/components/Chart/Index'
+import VaultsInfo from '@/components/VaultsInfo'
+import Card from '@/components/common/Card'
 
-import Chart from '../Chart/Index'
-import ChartOptions from '../ChartOptions/Index'
+import CollateralsTable from '../CollateralsTable'
+import TradesTable from '../TradesTable'
 
 import * as d3 from 'd3'
 import { timeParse } from 'd3-time-format'
@@ -28,13 +25,9 @@ const parseData = (data: any) => {
 }
 
 const Home = () => {
-  const { coin } = useContext(CoinContext)
-  const [days, setDays] = useState<number>(2)
   const [prices, setPrices] = useState<number[][]>([])
   const [dates, setDates] = useState<Date[]>([])
   const [coins, setCoins] = useState<string[]>([])
-  const [volatility, setVolatility] = useState<number>(0.1)
-
   const fetchDaily = async () => {
     try {
       let dailyData = await d3.csv('/daily_prices_for_jesus.csv')
@@ -67,37 +60,28 @@ const Home = () => {
     fetchDaily()
   }, [])
 
-  const submit = (data: { volatility: SetStateAction<number>; days: SetStateAction<number> }) => {
-    setVolatility(data.volatility)
-    setDays(data.days)
-  }
-
   return (
     <>
-      <div className="relative flex space-around space-x-10 mt-[2vh] ">
-        <div className="w-full">
-          <div className="flex justify-between items-start z-100">
-            <h1 className="text-4xl pb-2">Overview</h1>
-            <div className="flex justify-start items-center space-x-5 mt-[1vh]">
-              <CryptoIcon coin={coin} className="size-[3vw]" type="white" />
-              <CoinSelect coins={coins} />
-            </div>
-          </div>
-          <div>
-            <ChartOptions onSubmit={submit} />
-            {prices.length > 0 ? (
-              <Chart window={days} volatility={volatility} coins={coins} prices={prices} dates={dates} />
-            ) : (
-              <p className="text-3xl">Loading...</p>
-            )}
-            <Products></Products>
-          </div>
+      <div className="grid grid-cols-11">
+        <div className="p-[1vw] col-span-8">
+          {prices.length > 0 ? (
+            <Chart coins={coins} prices={prices} dates={dates.slice(-365)} />
+          ) : (
+            <p className="text-3xl">Loading...</p>
+          )}
         </div>
-        <div className="w-min">
-          <ActionCard />
-          <div className="my-10">
-            <AssetInfo />
-          </div>
+        <div className="p-[1vw] col-span-3 ">
+          <ActionCard coins={coins} />
+        </div>
+        <div className="p-[1vw] col-span-8">
+          <Card className=" flex justify-between w-full">
+            <CollateralsTable />
+            <TradesTable />
+          </Card>
+        </div>
+
+        <div className="p-[1vw] col-span-3 ">
+          <VaultsInfo />
         </div>
       </div>
     </>
