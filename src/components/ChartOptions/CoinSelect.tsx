615546d89fc46b2fb591a3fe0f63944b8fcf2c07
Carl Gauss <carlgauss221@gmail.com>
2024-06-04 13:27:44 -0400

    FIX: changed padding on coin select

--- src/components/ChartOptions/CoinSelect.tsx
+++ src/components/ChartOptions/CoinSelect.tsx
@@ -7,7 +7,7 @@ const CoinSelect = ({ coins }: { coins: string[] }) => {
 
   return (
     <select
-      className="px-10 py-2 h-fit w-fit rounded-2xl text-md border border-white bg-smoke text-white text-left"
+      className="px-10 py-0.5 h-fit w-fit rounded-xl text-md border border-white bg-smoke text-white text-left"
       id="cryptoCoin"
       onChange={(e) => setCoin(e.target.value)}
       value={coin}
