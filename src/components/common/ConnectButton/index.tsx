a8eed98daa3a6ad919e066bee40555c58c9730a8
Carl Gauss <carlgauss221@gmail.com>
2024-06-04 13:34:21 -0400

    FIX: added classname to button and added disconnect address

--- src/components/common/ConnectButton/index.tsx
+++ src/components/common/ConnectButton/index.tsx
@@ -1,18 +1,27 @@
 import { useWeb3Modal } from '@web3modal/wagmi/react'
 
+import { Button } from '@mui/base'
+
 import { useAccount, useDisconnect } from 'wagmi'
 
-import ClearButton from '@/components/common/ClearButton'
+import { shortenAddress } from '@/utils/formatters'
 
 const ConnectButton = () => {
   const { open } = useWeb3Modal()
-  const { isConnected } = useAccount()
+  const { isConnected, address } = useAccount()
   const { disconnect } = useDisconnect()
 
   return (
-    <ClearButton handleClickClearButton={isConnected ? () => disconnect() : () => open()}>
-      {isConnected ? <p className="text-[.8vw]">Disconnect Wallet</p> : <p className="text-[1vw]">Connect Wallet</p>}
-    </ClearButton>
+    <Button
+      onClick={isConnected ? () => disconnect() : () => open()}
+      className="bg-smoke py-[2vh] px-[4vw] w-full flex justify-center rounded-xl text-white  hover:scale-110  hover:text-carmesi"
+    >
+      {isConnected ? (
+        <p className="text-[.8vw]">Disconnect {shortenAddress(address)}</p>
+      ) : (
+        <p className="text-[1vw]">Connect Wallet</p>
+      )}
+    </Button>
   )
 }
 
