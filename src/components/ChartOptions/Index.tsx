9b43df54abe49f2f60bc0f3fd342acddf54e10e1
Carl Gauss <carlgauss221@gmail.com>
2024-06-05 17:36:34 -0400

    ADDED: window pick to chart options

--- src/components/ChartOptions/Index.tsx
+++ src/components/ChartOptions/Index.tsx
@@ -1,9 +1,7 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 
-import { CoinContext } from '@/components/AppProviders'
-
-import CryptoIcon from '../common/CryptoIcon'
 import CoinSelect from './CoinSelect'
+import DatePicker from './DatePicker'
 import SetDays from './SetDays'
 import SetVolatility from './SetVolatility'
 
@@ -17,35 +15,47 @@ interface dataType {
   end: Date
   volatility: number
   days: number
+  dates: number
 }
 
 const ChartOptions: React.FC<ChartOptionsProps> = ({ onSubmit, coins }) => {
   const [volatility, setVolatility] = useState<number>(0.2)
   const [days, setDays] = useState<number>(14)
-  const { coin } = useContext(CoinContext)
+  const [dates, setDates] = useState<number>(365)
 
   useEffect(() => {
     const debounceSubmit = setTimeout(() => {
       const data = {
         volatility,
         days,
+        dates,
       }
       onSubmit(data as dataType)
     }, 500)
 
     return () => clearTimeout(debounceSubmit)
-  }, [days, onSubmit, volatility])
+  }, [days, onSubmit, volatility, dates])
 
   return (
-    <div className="w-full h-[10vh] flex justify-start items-center space-x-10">
-      <div className="flex justify-start items-center">
-        <CryptoIcon coin={coin} className="size-[2vw] mr-[1vw]" type="white" />
+    <div className="w-full flex justify-end items-center space-x-[2vw] pr-[2vw]">
+      <div className="block space-y-[1vh]">
+        <p className="opacity-30 text-right">Asset:</p>
         <CoinSelect coins={coins} />
       </div>
+      <div className="block space-y-[1vh]">
+        <p className="opacity-30 text-right">Window:</p>
+        <DatePicker setDates={setDates} />
+      </div>
 
-      <p className="border px-10 py-0.5 bg-smoke">Last 365 days</p>
-      <SetDays days={days} setDays={setDays} />
-      <SetVolatility setVolatility={setVolatility} />
+      <div className="block space-y-[1vh]">
+        <p className="opacity-30 text-right">Rolling Window:</p>
+        <SetDays days={days} setDays={setDays} />
+      </div>
+
+      <div className="block space-y-[1vh]">
+        <p className="opacity-30 text-right">Volatility:</p>
+        <SetVolatility setVolatility={setVolatility} />
+      </div>
     </div>
   )
 }
