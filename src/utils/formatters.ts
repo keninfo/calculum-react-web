c1e5d42eb9e0cc5a260ac26486fbf2faa9cbb252
Carl Gauss <carlgauss221@gmail.com>
2024-06-05 17:49:18 -0400

    FIXED: decimal error

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -16,10 +16,35 @@ export const formatBalance = (number: bigint): string => {
 
     if (length > 6) {
       integerPart = toStringNumber.slice(0, length - 6)
-      decimalPart = toStringNumber.slice(length - 6, length - 6)
+      decimalPart = toStringNumber.slice(length - 6, length - 4) // Capture two decimal digits
     } else {
       integerPart = '0'
-      decimalPart = '0'.repeat(18 - length) + toStringNumber.slice(0, 2)
+      decimalPart = '0'.repeat(6 - length) + toStringNumber // Adjust for cases where length <= 6
+    }
+
+    let formattedNumber = `${integerPart}.${decimalPart}`
+    formattedNumber = parseFloat(formattedNumber).toFixed(2)
+
+    return formattedNumber
+  } catch (error) {
+    return '0.0'
+  }
+}
+
+export const formatShares = (share: bigint): string => {
+  try {
+    const toStringNumber = share.toString()
+    const length = toStringNumber.length
+
+    let integerPart = '0'
+    let decimalPart = '0'
+
+    if (length > 18) {
+      integerPart = toStringNumber.slice(0, length - 18)
+      decimalPart = toStringNumber.slice(length - 18, length - 16) // Capture two decimal digits
+    } else {
+      integerPart = '0'
+      decimalPart = '0'.repeat(18 - length) + toStringNumber // Adjust for cases where length <= 18
     }
 
     let formattedNumber = `${integerPart}.${decimalPart}`
@@ -52,29 +77,4 @@ export const formatPercentage = (percentage: number): string => {
   const formattedPercentage = `${prefix}${roundedPercentage}%`
 
   return formattedPercentage
-}
-
-export const formatShares = (share: bigint): string => {
-  try {
-    const toStringNumber = share.toString()
-    const length = toStringNumber.length
-
-    let integerPart = '0'
-    let decimalPart = '0'
-
-    if (length > 18) {
-      integerPart = toStringNumber.slice(0, length - 18)
-      decimalPart = toStringNumber.slice(length - 18, length - 18)
-    } else {
-      integerPart = '0'
-      decimalPart = '0'.repeat(18 - length) + toStringNumber.slice(0, 2)
-    }
-
-    let formattedNumber = `${integerPart}.${decimalPart}`
-    formattedNumber = parseFloat(formattedNumber).toFixed(2)
-
-    return formattedNumber
-  } catch (error) {
-    return '0.0'
-  }
 }
