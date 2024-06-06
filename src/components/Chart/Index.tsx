0e60b2a718584031dee0cacb178e8328ae8d0026
Carl Gauss <carlgauss221@gmail.com>
2024-06-06 11:51:24 -0400

    ADDED: tooltip to chart

--- src/components/Chart/Index.tsx
+++ src/components/Chart/Index.tsx
@@ -10,7 +10,7 @@ import Card from '@/components/common/Card'
 import { pct_change, calculateScaledReturns, calculateCumulativeReturns, calculateRolling } from './chartComputations'
 
 import type { IChartApi, Time } from 'lightweight-charts'
-import { createChart, ColorType, LineStyle } from 'lightweight-charts'
+import { createChart, ColorType } from 'lightweight-charts'
 
 interface ChartDataPrice {
   time: Time
@@ -73,44 +73,53 @@ const Chart = ({ coins, prices, dates }: { coins: string[]; prices: number[][]; 
       chartInstance.current.remove()
     }
 
-    chartInstance.current = createChart(chartContainerRef.current, {
-      height: 400,
-      layout: {
-        background: { type: ColorType.Solid, color: 'transparent' },
-        textColor: 'white',
-      },
-      grid: {
-        vertLines: {
-          visible: false,
+    if (chartContainerRef.current) {
+      chartInstance.current = createChart(chartContainerRef.current, {
+        height: 400,
+        layout: {
+          background: { type: ColorType.Solid, color: 'transparent' },
+          textColor: 'white',
         },
-        horzLines: {
-          visible: false,
+        grid: {
+          vertLines: {
+            visible: false,
+          },
+          horzLines: {
+            visible: false,
+          },
         },
-      },
-      rightPriceScale: {
-        visible: true,
-        borderVisible: false,
-      },
-      leftPriceScale: {
-        mode: 2,
-        visible: true,
-        borderVisible: false,
-      },
-      timeScale: {
-        visible: true,
-        borderVisible: false,
-        fixLeftEdge: true,
-      },
-      crosshair: {
-        vertLine: {
-          color: '#C3BCDB44',
-          style: LineStyle.Solid,
+        rightPriceScale: {
+          visible: true,
+          borderVisible: false,
         },
-        horzLine: {
-          color: 'white',
+        leftPriceScale: {
+          mode: 0,
+          visible: true,
+          borderVisible: false,
         },
-      },
-    })
+        timeScale: {
+          visible: true,
+          borderVisible: false,
+          fixLeftEdge: true,
+        },
+        crosshair: {
+          horzLine: {
+            visible: false,
+            labelVisible: false,
+          },
+          vertLine: {
+            visible: true,
+            style: 0,
+            width: 2,
+            color: 'rgba(32, 38, 46, 0.1)',
+            labelVisible: false,
+          },
+        },
+        localization: {
+          dateFormat: "dd MMMM 'yy",
+        },
+      })
+    }
 
     const datesFiltered = dates.slice(-last)
     const visibleRange = {
@@ -118,32 +127,32 @@ const Chart = ({ coins, prices, dates }: { coins: string[]; prices: number[][]; 
       to: formatDate(datesFiltered[datesFiltered.length - 1]) as Time,
     }
 
-    const lineSeries = chartInstance.current.addLineSeries({
-      color: 'white',
+    const lineSeries = chartInstance.current?.addLineSeries({
+      color: '#ef233c',
       priceScaleId: 'left',
     })
 
     const chartDataPrice: ChartDataPrice[] = cumulativeReturnsScaled.map((data, index) => ({
       time: formatDate(datesFiltered[index]) as Time,
-      value: data,
+      value: (data - 1) * 100,
     }))
 
-    lineSeries.setData(chartDataPrice)
+    lineSeries?.setData(chartDataPrice)
 
-    const lineSeries2 = chartInstance.current.addLineSeries({
-      color: '#ef233c',
+    const lineSeries2 = chartInstance.current?.addLineSeries({
+      color: 'white',
       priceScaleId: 'left',
     })
 
     const chartDataPrice2: ChartDataPrice[] = cumulativeReturns_ret.map((data, index) => ({
       time: formatDate(datesFiltered[index]) as Time,
-      value: data,
+      value: (data - 1) * 100,
     }))
 
-    lineSeries2.setData(chartDataPrice2)
+    lineSeries2?.setData(chartDataPrice2)
 
     // Set visible range after chart is created
-    chartInstance.current.timeScale().setVisibleRange(visibleRange)
+    chartInstance.current?.timeScale().setVisibleRange(visibleRange)
 
     const rightLabel = document.createElement('div')
     rightLabel.style.position = 'absolute'
@@ -153,11 +162,101 @@ const Chart = ({ coins, prices, dates }: { coins: string[]; prices: number[][]; 
     rightLabel.style.zIndex = '1'
     rightLabel.innerText = 'ROC (%)'
 
-    chartContainerRef.current.appendChild(rightLabel)
+    chartContainerRef.current?.appendChild(rightLabel)
+
+    // Tooltip setup
+    const toolTipWidth = 96
+
+    // Create and style the tooltip html element
+    const toolTip = document.createElement('div')
+    Object.assign(toolTip.style, {
+      width: `${toolTipWidth}px`,
+      height: '400px',
+      position: 'absolute',
+      display: 'none',
+      padding: '8px',
+      boxSizing: 'border-box',
+      fontSize: '12px',
+      textAlign: 'left',
+      zIndex: '1000',
+      top: '12px',
+      left: '12px',
+      pointerEvents: 'none',
+      fontFamily: '-apple-system, BlinkMacSystemFont, Montserrat, Roboto, Ubuntu, sans-serif',
+      color: 'white',
+    })
+
+    toolTip.style.background = `rgba(255, 255, 255, 0.25)`
+    toolTip.style.color = 'white'
+    chartContainerRef.current?.appendChild(toolTip)
+
+    chartInstance.current?.subscribeCrosshairMove((param) => {
+      if (
+        param.point === undefined ||
+        !param.time ||
+        param.point.x < 0 ||
+        param.point.x > (chartContainerRef.current?.clientWidth ?? 0) ||
+        param.point.y < 0 ||
+        param.point.y > (chartContainerRef.current?.clientHeight ?? 0)
+      ) {
+        toolTip.style.display = 'none'
+      } else {
+        toolTip.style.display = 'block'
+        const dateStr = param.time as string
+        const data = lineSeries ? (param.seriesData.get(lineSeries) as { value?: number; close?: number }) : undefined
+        const rocScaled = data?.value !== undefined ? data.value : data?.close
+        const data2 = lineSeries2
+          ? (param.seriesData.get(lineSeries2) as { value?: number; close?: number })
+          : undefined
+        const rocCumulative = data2?.value !== undefined ? data2.value : data2?.close
+
+        if (rocCumulative !== undefined && rocScaled !== undefined) {
+          if (rocCumulative < rocScaled) {
+            toolTip.innerHTML = `<div style="color: white">${coin}</div>
+            <div>
+              <p style="font-size: 16px; margin: 4px 0px; color: #ef233c; font-weight: bold;">
+                ${rocScaled?.toFixed(2)}%</p>
+              <p style="font-size: 16px; margin: 4px 0px; color: white; font-weight: bold;">
+                ${rocCumulative?.toFixed(2)}%
+              </p>
+            </div>
+            <div style="position: absolute; bottom: 0; left: 0; width: 100%; background-color: #161a1d; color: white; text-align: center; padding-top: 4px; padding-bottom: 8px;">
+              ${dateStr}
+            </div>
+            `
+          } else {
+            toolTip.innerHTML = `<div style="color: white">${coin}</div>
+            <div>
+              <p style="font-size: 16px; margin: 4px 0px; color: 'white'; font-weight: bold;">
+                ${rocCumulative?.toFixed(2)}%</p>
+              <p style="font-size: 16px; margin: 4px 0px; color:#ef233c; font-weight: bold;">
+                ${rocScaled?.toFixed(2)}%
+              </p>
+            </div>
+            <div style="position: absolute; bottom: 0; left: 0; width: 100%; background-color: #161a1d; color: white; text-align: center; padding-top: 4px; padding-bottom: 8px;">
+              ${dateStr}
+            </div>
+            `
+          }
+
+          let left = param.point.x as number
+          const timeScaleWidth = chartInstance.current?.timeScale().width() ?? 0
+          const priceScaleWidth = chartInstance.current?.priceScale('left').width() ?? 0
+          const halfTooltipWidth = toolTipWidth / 2
+          left += priceScaleWidth - halfTooltipWidth
+          left = Math.min(left, priceScaleWidth + timeScaleWidth - toolTipWidth)
+          left = Math.max(left, priceScaleWidth)
+
+          toolTip.style.left = left + 'px'
+          toolTip.style.top = '0px'
+        }
+      }
+    })
 
     return () => {
       if (chartInstance.current) {
         chartInstance.current.remove()
+        toolTip.style.display = 'none'
         chartInstance.current = undefined
       }
     }
