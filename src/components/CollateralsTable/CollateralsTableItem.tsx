e1e30ee1e87e4c314d9329b04ecc6a171f934903
Carl Gauss <carlgauss221@gmail.com>
2024-06-07 02:22:53 -0400

    FIXED: collaterals table to show actual collateral (USDC only for now)

--- src/components/CollateralsTable/CollateralsTableItem.tsx
+++ src/components/CollateralsTable/CollateralsTableItem.tsx
@@ -1,26 +1,15 @@
-import React, { useContext } from 'react'
+import React from 'react'
 
-import { CoinContext } from '@/components/AppProviders'
 import CryptoIcon from '@/components/common/CryptoIcon'
+import { formatBalance } from '@/utils/formatters'
 
-import type { coinData } from './sampleData'
-
-const CollateralsTableItem = (element: coinData) => {
-  const { coin, setCoin } = useContext(CoinContext)
-
-  const handleTableRowClick = (element: coinData) => {
-    setCoin(element.asset)
-  }
+const CollateralsTableItem = (amount: bigint) => {
   return (
-    <tr
-      key={element.asset}
-      className="cursor-pointer hover:scale-105 text-sm [&>td]:py-[2vh] [&>td]:border-b [&>td]:border-darkness"
-      onClick={() => handleTableRowClick(element)}
-    >
-      <td className={`flex justify-start items-center mr-[4vw] ${element.asset === coin ? 'text-carmesi' : ''}`}>
-        <CryptoIcon coin={element.asset} className="size-[1.5rem] mr-[.5vw]" /> {element.asset}
+    <tr className="cursor-pointer hover:scale-105 text-sm [&>td]:py-[2vh] [&>td]:border-b [&>td]:border-darkness">
+      <td className={`flex justify-start items-center mr-[4vw] `}>
+        <CryptoIcon coin={'USDC'} className="size-[1.5rem] mr-[.5vw]" /> USDC
       </td>
-      <td className="text-right">${element.price.toFixed(2)}</td>
+      <td className="text-right">${formatBalance(amount)}</td>
     </tr>
   )
 }
--- src/components/CollateralsTable/index.tsx
+++ src/components/CollateralsTable/index.tsx
@@ -1,25 +1,19 @@
-import React, { useContext } from 'react'
+import React from 'react'
 
-import { CoinContext } from '@/components/AppProviders'
+import ContractReads from '@/hooks/useContractReads'
 
 import CollateralsTableItem from './CollateralsTableItem'
-import { sampleData } from './sampleData'
 
 const CollateralsTable = () => {
-  const { coin } = useContext(CoinContext)
+  const { TotalAssets } = ContractReads()
 
-  sampleData.forEach((item, i) => {
-    if (item.asset === coin) {
-      sampleData.splice(i, 1)
-      sampleData.unshift(item)
-    }
-  })
+  const total = TotalAssets().data as bigint
 
   return (
     <div className="border-r pr-[5vh]">
       <h1 className="text-2xl mb-[2vh] text-center">COLLATERAL</h1>
       <table className="table-fixed text-left mx-[1vw]">
-        <tbody className="text-2xl">{sampleData.map((element) => CollateralsTableItem(element))}</tbody>
+        <tbody className="text-2xl">{CollateralsTableItem(total)}</tbody>
       </table>
     </div>
   )
