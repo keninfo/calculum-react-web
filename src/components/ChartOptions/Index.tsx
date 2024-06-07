066e718d237ac97659af1f1b3282d6cd39a1223d
Carl Gauss <carlgauss221@gmail.com>
2024-06-07 02:25:42 -0400

    FIXED: set default parameters for vol and window

--- src/components/ChartOptions/Index.tsx
+++ src/components/ChartOptions/Index.tsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 
 import CoinSelect from './CoinSelect'
 import DatePicker from './DatePicker'
-import SetDays from './SetDays'
-import SetVolatility from './SetVolatility'
 
 interface ChartOptionsProps {
   onSubmit: (dataType: dataType) => void
   coins: string[]
+  volatility: number
+  days: number
 }
 
 interface dataType {
@@ -18,54 +18,52 @@ interface dataType {
   dates: number
 }
 
-const ChartOptions: React.FC<ChartOptionsProps> = ({ onSubmit, coins }) => {
-  const [volatility, setVolatility] = useState<number>(0.2)
-  const [days, setDays] = useState<number>(14)
+const ChartOptions: React.FC<ChartOptionsProps> = ({ onSubmit, coins, volatility, days }) => {
   const [dates, setDates] = useState<number>(365)
 
   useEffect(() => {
     const debounceSubmit = setTimeout(() => {
       const data = {
-        volatility,
-        days,
         dates,
       }
       onSubmit(data as dataType)
     }, 500)
 
     return () => clearTimeout(debounceSubmit)
-  }, [days, onSubmit, volatility, dates])
+  }, [onSubmit, dates])
 
   return (
-    <div className="flex justify-between items-end">
+    <div className="flex justify-between items-start -mb-[5vh] w-full">
       <div className="inline w-full">
         <div className="flex items-center space-x-2">
           <div className="w-[2vw] h-1 bg-white"></div>
-          <span className="text-xs">BTC - Return on Capital (RoC)</span>
+          <span className="text-sm">BTC - Return on Capital (RoC)</span>
         </div>
         <div className="flex items-center space-x-2 w-fit">
           <div className="w-[2vw] h-1 bg-carmesi"></div>
-          <p className="text-carmesi text-xs">BTC Vol Scaled - Return on Capital (RoC) </p>
+          <p className="text-carmesi text-sm">BTC Vol Scaled - Return on Capital (RoC) </p>
         </div>
       </div>
-      <div className="w-full flex justify-end items-center space-x-[1vw] pr-[3vw]">
-        <div className="block space-y-[1vh]">
-          <p className="opacity-30 text-right text-sm">Asset:</p>
-          <CoinSelect coins={coins} />
+      <div className="w-full">
+        <div className="w-full flex justify-end items-center space-x-[1vw] pr-[3vw]">
+          <div className="block space-y-[1vh]">
+            <p className="opacity-30 text-right text-sm">Asset:</p>
+            <CoinSelect coins={coins} />
+          </div>
+          <div className="block space-y-[1vh]">
+            <p className="opacity-30 text-right text-sm">Data History:</p>
+            <DatePicker setDates={setDates} />
+          </div>
         </div>
-        <div className="block space-y-[1vh]">
-          <p className="opacity-30 text-right text-sm">Window:</p>
-          <DatePicker setDates={setDates} />
-        </div>
-
-        <div className="block space-y-[1vh]">
-          <p className="opacity-30 text-right text-sm">Rolling Window:</p>
-          <SetDays days={days} setDays={setDays} />
-        </div>
-
-        <div className="block space-y-[1vh]">
-          <p className="opacity-30 text-right text-sm">Volatility:</p>
-          <SetVolatility setVolatility={setVolatility} />
+        <div className="w-full flex justify-end items-center space-x-[1vw] pr-[3vw] mt-[3vh]">
+          <div className="flex text-greySmoke space-x-2">
+            <p>Volatility:</p>
+            <p>{volatility * 100}%</p>
+          </div>
+          <div className="flex text-greySmoke space-x-2">
+            <p>Rolling Window:</p>
+            <p>{days} days</p>
+          </div>
         </div>
       </div>
     </div>
