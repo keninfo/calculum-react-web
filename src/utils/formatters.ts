caf4210f2b06adddb8a5a1272f43e783847b4037
Carl Gauss <carlgauss221@gmail.com>
2024-06-11 23:04:11 -0400

    ADDED: format date to formatters util

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -78,3 +78,11 @@ export const formatPercentage = (percentage: number): string => {
 
   return formattedPercentage
 }
+
+export const formatDate = (date: Date): string => {
+  const newDate = new Date(date)
+  const year = newDate.getUTCFullYear()
+  const month = String(newDate.getUTCMonth() + 1).padStart(2, '0')
+  const day = String(newDate.getUTCDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
+}
