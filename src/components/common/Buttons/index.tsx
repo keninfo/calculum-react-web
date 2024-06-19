8de6b4c722b918adab8106816e2f9662b89dbadc
Carl Gauss <carlgauss221@gmail.com>
2024-06-19 16:10:29 -0400

    ADDED: buttons component

---
+++ src/components/common/Buttons/index.tsx
@@ -0,0 +1,39 @@
+import type { ReactNode } from 'react'
+
+type ButtonProps = {
+  handleClick: () => void
+  children: ReactNode
+  disabled?: boolean
+  className?: string
+  border?: boolean
+}
+
+export const PrimaryButton = ({ handleClick, children, disabled, border, className }: ButtonProps) => (
+  <button
+    onClick={handleClick}
+    className={`bg-smoke py-6 flex justify-center text-white  hover:scale-110 w-[100%] hover:text-carmesi ${border ? 'border-2' : ''} ${className}`}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+)
+
+export const SecondaryButton = ({ handleClick, children, disabled, border, className }: ButtonProps) => (
+  <button
+    onClick={handleClick}
+    className={`bg-darkness py-6 flex justify-center text-white  hover:scale-110 w-[100%] hover:text-carmesi ${border ? 'border-2' : ''} ${className}`}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+)
+
+export const AlternateButton = ({ handleClick, children, disabled, border, className }: ButtonProps) => (
+  <button
+    onClick={handleClick}
+    className={`bg-carmesi px-[2vw] py-[1vh] flex justify-center items-center text-white ${border ? 'border-2' : ''} ${className}`}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+)
