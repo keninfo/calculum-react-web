6b36a970078cdbfe85b4a652b9460b6c72e85a9c
Carl Gauss <carlgauss221@gmail.com>
2024-06-26 16:43:29 -0400

    REFACTOR: turned svg icons into components

---
+++ src/components/common/Icons/BearPassive.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const BearPassive = ({ className }: { className: string }) => {
+  return (
+    <svg
+      width="100%"
+      height="100%"
+      viewBox="0 0 1532 1306"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      style={{ fillRule: 'evenodd', clipRule: 'evenodd', strokeLinejoin: 'round', strokeMiterlimit: 2 }}
+      className={`fill-white ${className}`}
+    >
+      <path d="M198.476,1168.2l-2.039,-1.177l226.531,-72.435l213.436,50.023l188.046,-69.782l45.121,40.812l126.286,-64.405l142.877,51.045l146.93,3.041l84.314,-25.436l83.692,-24.512l77.726,-70.948l-106.753,-106.527l-101.922,-101.774l-68.165,-205.355l-86.305,-129.83l-240.539,-107.321l-8.629,-134.154l-77.061,-79.292l-119.702,2.748l-65.828,117.449l-170.829,-34.543l-117.303,-78.097l-368.36,-127.732l0,1305.55l198.476,-137.348Z" />
+    </svg>
+  )
+}
+
+export default BearPassive
