2c2919f159963e01f4acde051a25f515c990de44
Carl Gauss <carlgauss221@gmail.com>
2024-06-27 14:16:10 -0400

    ADDED: select component

--- src/components/common/Icons/Chevron.tsx
+++ src/components/common/Icons/Chevron.tsx
@@ -5,9 +5,17 @@ interface ChevronIconProps {
   stroke?: string
 }
 
-const Chevron: React.FC<ChevronIconProps> = ({ fill = 'none', stroke = 'white' }) => {
+const Chevron: React.FC<ChevronIconProps> = () => {
   return (
-    <svg fill={fill} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke={stroke} width="24" height="24">
+    <svg
+      fill="current"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      stroke="current"
+      width="14"
+      height="14"
+      className="fill-none stroke-white"
+    >
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
     </svg>
   )
