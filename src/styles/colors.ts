7a3aa4bae91f2782e074c07b1be9c5cdd5a4a202
Carl Gauss <carlgauss221@gmail.com>
2024-06-28 16:39:11 -0400

    REMOVED: light theme

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,16 +1,15 @@
 export const classicTheme = {
-  darkness: '#F9FAF9',
-  smoke: '#E8EBED',
+  darkness: '#161A1D',
+  smoke: '#222222',
   carmesi: '#EF233C',
-  white: '#353839',
-  greySmoke: '#999999',
+  white: '#FFFFFF',
+  greySmoke: '#666666',
 }
 
 export const proTheme = {
   darkness: '#161A1D',
   smoke: '#111111',
   carmesi: '#EF233C',
-  // carmesi: '#DAA520',
   white: '#FFFFFF',
   greySmoke: '#666666',
 }
