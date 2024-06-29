df00d8d410c7c2d66fe4b9ac36eb7ece7b83546c
Carl Gauss <carlgauss221@gmail.com>
2024-06-28 21:06:37 -0400

    ADDED: light Theme (unused)

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -13,3 +13,11 @@ export const proTheme = {
   white: '#FFFFFF',
   greySmoke: '#666666',
 }
+
+export const lightTheme = {
+  darkness: '#F9FAF9',
+  smoke: '#E8EBED',
+  carmesi: '#EF233C',
+  white: '#353839',
+  greySmoke: '#999999',
+}
