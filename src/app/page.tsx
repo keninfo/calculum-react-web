d8214ad6f31c72c170b4f831d4e8ed842db8af6a
Carl Gauss <carlgauss221@gmail.com>
2024-06-28 21:07:17 -0400

    REFACTOR: intro questionnaire

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client'
 
-import { useContext } from 'react'
-
 import Head from 'next/head'
 
-import { ProContext } from '@/components/AppProviders'
 import IntroQuestionnaire from '@/components/IntroQuestionnaire'
 import MetaTags from '@/components/common/MetaTags'
 
 const BearProtocolApp = () => {
-  const { pro } = useContext(ProContext)
   return (
     <>
       <Head>
@@ -18,16 +14,6 @@ const BearProtocolApp = () => {
       </Head>
 
       <main>
-        <img
-          src="/bearPassive.svg"
-          alt="Bear Protocol"
-          className={`hidden | md:block h-[20vw] absolute -left-20 top-1/2 -translate-y-10  z-50 ${pro ? 'opacity-20' : 'opacity-100'}`}
-        />
-        <img
-          src="/bearAttack.svg"
-          alt="Bear Protocol"
-          className={`hidden | md:block h-[20vw] absolute right-0 top-1/2  z-50 -scale-x-100 ${pro ? 'opacity-100' : 'opacity-20'}`}
-        />
         <IntroQuestionnaire />
       </main>
     </>
--- src/components/IntroQuestionnaire/index.tsx
+++ src/components/IntroQuestionnaire/index.tsx
@@ -1,39 +1,21 @@
-import { useContext, useEffect, useState } from 'react'
+import { useState } from 'react'
 
 import Link from 'next/link'
 
 import ContractReads from '@/hooks/useContractReads'
 
-import { ProContext } from '../AppProviders'
+import { PrimaryButton } from '../common/Buttons'
 import Card from '../common/Card'
 
-const experiences = [
-  { label: 'I have no experience.', value: 0 },
-  { label: 'I only have experience with one of the two.', value: 1 },
-  { label: 'I have experience with both.', value: 2 },
-]
-
-const vaultsKnowledge = [
-  { label: "I don't know what vaults are.", value: 0 },
-  { label: 'I have only heard of vaults.', value: 1 },
-  { label: 'I am familiar with vaults.', value: 2 },
-]
-
-const bearProtocolKnowledge = [
-  { label: 'First time hearing about Bear Protocol.', value: 0 },
-  { label: 'I have used Bear Protocol before.', value: 1 },
-  { label: "I'm an expert using Bear Protocol.", value: 2 },
+const options = [
+  `I want to control my portfolio's volatility but without giving away upside.`,
+  `Cool branding, i'm just curious about what $bear is.`,
+  `I'm here for the tokens.`,
 ]
 
 const IntroQuestionnaire = () => {
   const { InMaintenance } = ContractReads()
-  const [selectedExperience, setSelectedExperience] = useState(experiences[0].value)
-  const [selectedVaultKnowledge, setSelectedVaultKnowledge] = useState(vaultsKnowledge[0].value)
-  const [selectedBearProtocolKnowledge, setSelectedBearProtocolKnowledge] = useState(bearProtocolKnowledge[0].value)
-  const { setPro } = useContext(ProContext)
-
-  const totalScore = selectedExperience + selectedVaultKnowledge + selectedBearProtocolKnowledge
-
+  const [selected, setSelected] = useState<number>(1)
   let status = false
 
   const data = InMaintenance().data as [boolean, number]
@@ -41,69 +23,41 @@ const IntroQuestionnaire = () => {
     status = data[0] as boolean
   }
 
-  useEffect(() => {
-    if (totalScore < 3) {
-      setPro(false)
-    } else {
-      setPro(true)
-    }
-  }, [setPro, totalScore])
-
   return (
     <Card
-      className={`flex relative mx-auto flex-col justify-center items-center z-50 pb-[20vh] | md:z-10 md:pb-[5vh] ${status ? 'md:mt-[20vh]' : 'md:mt-[15vh]'}`}
+      className={`flex relative mx-auto flex-col justify-center items-center z-20 px-[5vw] w-screen h-screen | md:h-fit md:w-[60vw] ${status ? 'md:mt-[20vh]' : 'md:mt-[15vh]'}`}
     >
-      <Link href="/dashboard" className="absolute top-[3vh] right-1/2 translate-x-1/2 font-bold | md:right-[6vh]">
-        SKIP
-      </Link>
-      <h2 className="text-center text-carmesi font-bold text-xl mt-[6vh] | md:text-3xl md:my-2">
-        Welcome to Bear Protocol
-      </h2>
-      <p className="my-[4vh] font-bold text-center text-[2vh] | md:w-[50%] ">
-        Before continuing, please answer the following questions so we can offer the best experience customized to you:{' '}
-      </p>
-      <p className="text-center mb-2 text-sm text-greySmoke">What is your experience with trading and crypto?</p>
-      <select
-        className="px-2 py-1 text-[2vh] border bg-smoke text-white mb-4 text-center w-full | md:w-[50%]"
-        id="experience"
-        onChange={(e) => setSelectedExperience(Number(e.target.value))}
-        value={selectedExperience}
-      >
-        {experiences.map((experience, index) => (
-          <option key={index} value={experience.value}>
-            {experience.label}
-          </option>
-        ))}
-      </select>
+      <h2 className="text-center text-white text-xl">Hello Anon, Welcome to Bear Protocol</h2>
+      <h2 className="text-center text-carmesi text-xl mb-[6vh]">Why are you here?</h2>
+      <div className="flex justify-center items-start w-full space-x-[5vw]">
+        <PrimaryButton
+          handleClick={() => setSelected(0)}
+          className={`!p-0 !rounded-2xl overflow-hidden w-[100%] hover:scale-105 ${selected == 0 ? 'border-4 border-carmesi' : 'opacity-30'}`}
+        >
+          <div>
+            <img src="/bearHammock.png" alt="pic" />
+          </div>
+        </PrimaryButton>
+        <PrimaryButton
+          handleClick={() => setSelected(1)}
+          className={`!p-0 !rounded-2xl overflow-hidden w-[100%] hover:scale-105 ${selected == 1 ? 'border-4 border-carmesi' : 'opacity-30'}`}
+        >
+          <div>
+            <img src="/bearBeach.png" alt="pic" />
+          </div>
+        </PrimaryButton>
+        <PrimaryButton
+          handleClick={() => setSelected(2)}
+          className={`!p-0 !rounded-2xl overflow-hidden w-[100%] hover:scale-105 ${selected == 2 ? 'border-4 border-carmesi' : 'opacity-30'}`}
+        >
+          <div>
+            <img src="/bearToken.png" alt="pic" />
+          </div>
+        </PrimaryButton>
+      </div>
+      <p className="py-[6vh] font-bold text-lg text-center">{options[selected]}</p>
 
-      <p className="text-center mb-2 text-sm text-greySmoke">What do you know about vaults?</p>
-      <select
-        className="px-2 py-1 text-[2vh] border bg-smoke text-white mb-4 text-center w-full | md:w-[50%]"
-        id="vaultsKnowledge"
-        onChange={(e) => setSelectedVaultKnowledge(Number(e.target.value))}
-        value={selectedVaultKnowledge}
-      >
-        {vaultsKnowledge.map((knowledge, index) => (
-          <option key={index} value={knowledge.value}>
-            {knowledge.label}
-          </option>
-        ))}
-      </select>
-
-      <p className="text-center mb-2 text-sm  text-greySmoke">What do you know about Bear Protocol?</p>
-      <select
-        className="px-2 py-1 text-[2vh] border bg-smoke text-white mb-4 text-center w-full | md:w-[50%]"
-        id="bearProtocolKnowledge"
-        onChange={(e) => setSelectedBearProtocolKnowledge(Number(e.target.value))}
-        value={selectedBearProtocolKnowledge}
-      >
-        {bearProtocolKnowledge.map((knowledge, index) => (
-          <option key={index} value={knowledge.value}>
-            {knowledge.label}
-          </option>
-        ))}
-      </select>
-      <Link href="/dashboard" className="mt-4 font-bold hover:scale-105 bg-carmesi px-[10vw] | md:px-[1vw] py-[1vh]">
+      <Link href="/dashboard" className="font-bold hover:scale-105 bg-carmesi px-[10vw]">
         Submit
       </Link>
     </Card>
