e654e1d74a80e19f9f48db4f5fae5ba2e2760c21
Carl Gauss <carlgauss221@gmail.com>
2024-06-30 17:29:56 -0400

    FORCE UPDATE

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -74,7 +74,7 @@ const Home = () => {
       setDates(dates)
       setPrices(arrayOfArrays)
     } catch (error) {
-      console.error('Error fetching data:', error)
+      console.error('Error fetching data :', error)
     }
   }
 
