919d9863c9e52eb3472f9f23726e7908826065e4
Carl Gauss <carlgauss221@gmail.com>
2024-08-05 04:29:02 -0400

    ADDED: wallet balance to approve

--- src/components/Deposit/Approve.tsx
+++ src/components/Deposit/Approve.tsx
@@ -2,22 +2,33 @@ import React, { useEffect, useState } from 'react'
 
 import Link from 'next/link'
 
-import { type BaseError } from 'wagmi'
+import { type BaseError, useAccount } from 'wagmi'
 
 import { PrimaryButton } from '@/components/common/Buttons'
 import useApprove from '@/hooks/useApprove'
+import ContractReads from '@/hooks/useContractReads'
 import createTransactionAlert from '@/utils/createTransactionAlert'
+import { formatBalance } from '@/utils/formatters'
 
 import Input from '../common/Input'
 
 const Approve = ({ onApprove }: { onApprove: () => void }) => {
   const { ApproveAssets, isPending, error, hash } = useApprove()
+  const { SymbolAsset, BalanceAssets } = ContractReads()
   const [amount, setAmount] = useState<number>(0)
+  const [formattedBalance, setFormattedBalance] = useState<number>(0)
+  const { address } = useAccount()
+
+  const balanceAssets = BalanceAssets(address).data as bigint
 
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseFloat(e.target.value)
     setAmount(value)
   }
+
+  useEffect(() => {
+    setFormattedBalance(parseFloat(formatBalance(balanceAssets)))
+  }, [balanceAssets])
 
   useEffect(() => {
     if (hash) {
@@ -30,7 +41,17 @@ const Approve = ({ onApprove }: { onApprove: () => void }) => {
 
   return (
     <>
-      <p className="mb-[1vh] text-right text-xs">
+      <p className="mb-[1vh] mt-[2vh] text-left text-xs">
+        You have
+        <b className="text-carmesi mx-1">
+          {formattedBalance} {SymbolAsset().data as string}
+        </b>{' '}
+        in Wallet
+      </p>
+      <div className="flex justify-between">
+        <Input type="number" value={amount} handleChange={handleAmountChange} />
+      </div>
+      <p className="mt-[1vh] text-center text-xs">
         <Link
           href={'https://revoke.cash/learn/approvals/what-are-token-approvals'}
           target="_blank"
@@ -39,10 +60,7 @@ const Approve = ({ onApprove }: { onApprove: () => void }) => {
           Why do I have to approve?
         </Link>
       </p>
-      <div className="flex justify-between">
-        <Input type="number" value={amount} handleChange={handleAmountChange} />
-      </div>
-      <PrimaryButton handleClick={() => ApproveAssets(amount)} disabled={isPending} className="my-[4vh]">
+      <PrimaryButton handleClick={() => ApproveAssets(amount)} disabled={isPending} className="my-[2vh]">
         {isPending ? 'Approving...' : 'Approve'}
       </PrimaryButton>
     </>
