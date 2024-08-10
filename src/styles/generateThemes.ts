c70da68df0325b3f168073c80a7f93fba28e2b38
Carl Gauss <carlgauss221@gmail.com>
2024-08-10 15:06:15 -0400

    FIXED: Color variable generator

--- src/styles/generateThemes.ts
+++ src/styles/generateThemes.ts
@@ -3,31 +3,31 @@ import { classicTheme, proTheme, lightTheme } from './colors'
 import fs from 'fs'
 
 const generateCSS = () => {
-  const css = `
-    .classic {
-      --color-darkness: ${classicTheme.darkness};
-      --color-smoke: ${classicTheme.smoke};
-      --color-carmesi: ${classicTheme.carmesi};
-      --color-white: ${classicTheme.white};
-      --color-greySmoke: ${classicTheme.greySmoke};
-    }
+  const css = `/* DO NOT EDIT - Edit colors.ts instead and then npm run css-vars or yarn run css-vars */
+.classic {
+  --color-darkness: ${classicTheme.darkness};
+  --color-smoke: ${classicTheme.smoke};
+  --color-carmesi: ${classicTheme.carmesi};
+  --color-white: ${classicTheme.white};
+  --color-greySmoke: ${classicTheme.greySmoke};
+}
 
-    .pro {
-      --color-darkness: ${proTheme.darkness};
-      --color-smoke: ${proTheme.smoke};
-      --color-carmesi: ${proTheme.carmesi};
-      --color-white: ${proTheme.white};
-      --color-greySmoke: ${proTheme.greySmoke};
-    }
-    
-    .light {
-      --color-darkness: ${lightTheme.darkness};
-      --color-smoke: ${lightTheme.smoke};
-      --color-carmesi: ${lightTheme.carmesi};
-      --color-white: ${lightTheme.white};
-      --color-greySmoke: ${lightTheme.greySmoke};
-    }
-  `
+.pro {
+  --color-darkness: ${proTheme.darkness};
+  --color-smoke: ${proTheme.smoke};
+  --color-carmesi: ${proTheme.carmesi};
+  --color-white: ${proTheme.white};
+  --color-greySmoke: ${proTheme.greySmoke};
+}
+
+.light {
+  --color-darkness: ${lightTheme.darkness};
+  --color-smoke: ${lightTheme.smoke};
+  --color-carmesi: ${lightTheme.carmesi};
+  --color-white: ${lightTheme.white};
+  --color-greySmoke: ${lightTheme.greySmoke};
+}
+/* DO NOT EDIT - Edit colors.ts instead and then npm run css-vars or yarn run css-vars */`
 
   fs.writeFileSync('./src/styles/_theme.css', css)
 }
