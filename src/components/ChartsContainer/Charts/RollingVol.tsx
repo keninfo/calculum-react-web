f526aa01fe2ee6bbeb6856c64d9a710c2324560d
Carl Gauss <carlgauss221@gmail.com>
2024-08-19 17:35:24 -0400

    FIXED: rolling vol color

--- src/components/ChartsContainer/Charts/RollingVol.tsx
+++ src/components/ChartsContainer/Charts/RollingVol.tsx
@@ -110,7 +110,7 @@ const RollingVol = ({ dates, seriesData }: ChartProps) => {
     }
 
     const lineSeries = chartInstance.current?.addLineSeries({
-      color: '#64b6ac',
+      color: '#29947A',
       priceScaleId: 'left',
       autoscaleInfoProvider: () => ({
         priceRange: {
@@ -161,7 +161,7 @@ const RollingVol = ({ dates, seriesData }: ChartProps) => {
         if (rollingVol !== undefined) {
           toolTip.innerHTML = `<div style="color: var(--color-white)">${coin}</div>
           <div>
-            <p style="font-size: 10px; margin: 4px 0px; color: #64b6ac; font-weight: bold;">
+            <p style="font-size: 10px; margin: 4px 0px; color: #29947A; font-weight: bold;">
             Vol: ${rollingVol?.toFixed(2)}%</p>
           </div>
           <div style="position: absolute; bottom: 0; left: 0; width: 100%; background-color: var(--color-darkness); color: var(--color-white); text-align: center; padding-top: 4px; padding-bottom: 8px;">
