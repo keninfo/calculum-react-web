419fa43ca0cb2a1d6b7792f9625488f146cc9cd6
Carl Gauss <carlgauss221@gmail.com>
2024-08-23 16:04:46 -0400

    FIXED: buttons colors

--- src/components/common/Buttons/index.tsx
+++ src/components/common/Buttons/index.tsx
@@ -11,7 +11,7 @@ type ButtonProps = {
 export const PrimaryButton = ({ handleClick, children, disabled, border, className }: ButtonProps) => (
   <button
     onClick={handleClick}
-    className={`bg-smoke py-6 flex justify-center text-white  hover:scale-110 w-[100%] rounded-lg hover:text-carmesi ${border ? 'border-2' : ''} ${className}`}
+    className={`bg-carmesi py-6 flex justify-center text-white  hover:scale-110 w-[100%] rounded-lg hover:text-smoke ${border ? 'border-2' : ''} ${className}`}
     disabled={disabled}
   >
     {children}
@@ -31,7 +31,7 @@ export const SecondaryButton = ({ handleClick, children, disabled, border, class
 export const AlternateButton = ({ handleClick, children, disabled, border, className }: ButtonProps) => (
   <button
     onClick={handleClick}
-    className={`bg-carmesi px-[2vw] py-[1vh] flex justify-center items-center text-white rounded-lg   ${border ? 'border-2 border-white' : ''} ${className}`}
+    className={`bg-smoke px-[2vw] py-[1vh] flex justify-center items-center text-white rounded-lg hover:text-carmesi   ${border ? 'border-2 border-white' : ''} ${className}`}
     disabled={disabled}
   >
     {children}
