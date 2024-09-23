48273ab28be93b75b47cbb9b43b8e13a6a318733
Carl Gauss <carlgauss221@gmail.com>
2024-09-23 13:49:41 -0400

    REMOVED: mobile chartOptions (using desktop responsive for now )

--- src/components/ChartOptions/Index.tsx
+++ src/components/ChartOptions/Index.tsx
@@ -9,7 +9,6 @@ import Card from '@/components/common/Card'
 import CaseStudies from './CaseStudies'
 import CoinSelect from './CoinSelect'
 import SetWindow from './SetWindow'
-import ShowCandle from './ShowCandle'
 
 const ChartOptions = ({ guide = false }: { guide?: boolean }) => {
   const { volatility, rollingWindow, studyCase } = useContext(OptionsContext)
@@ -19,7 +18,7 @@ const ChartOptions = ({ guide = false }: { guide?: boolean }) => {
   return (
     <div>
       {/* DESKTOP */}
-      <Card className="hidden | md:block w-full mb-[2vh] relative">
+      <Card className="block w-full mb-[2vh] relative">
         <div className="flex justify-between items-center">
           <p className="text-greySmoke text-left text-sm">Asset:</p>
           <CoinSelect />
@@ -66,71 +65,6 @@ const ChartOptions = ({ guide = false }: { guide?: boolean }) => {
           {pro && !guide && <RebalancingResults />}
         </div>
       </Card>
-
-      {/* MOBILE */}
-      <div className="w-full pl-[2vw] | md:hidden">
-        <div className="w-full grid grid-cols-2 items-center mt-[6vh] px-6">
-          {/* <div className="col-span-1 mr-[2vw]">
-            <select
-              className="px-10 py-0.5 h-fit w-full text-sm border bg-smoke text-white text-left"
-              id="cryptoCoin"
-              value={'BETA'}
-            >
-              <option key={1} value={'BETA'}>
-                {'BETA'}
-              </option>
-            </select>
-          </div> */}
-          <div className="col-span-1 ml-[2vw]">
-            <CoinSelect />
-          </div>
-        </div>
-        <div className="w-full grid grid-cols-2 items-center mt-[2vw] px-6">
-          <div className="col-span-1 mr-[2vw]">
-            <ShowCandle />
-          </div>
-          <div className="col-span-1 ml-[2vw]">
-            {studyCase == 0 && <SetWindow />}
-            {studyCase == 1 && (
-              <p className="px-[2vw] py-0.5 h-fit w-full text-sm border  bg-smoke text-greySmoke text-left">
-                04/01/2021
-              </p>
-            )}
-            {studyCase == 2 && (
-              <p className="px-[2vw] py-0.5 h-fit w-full text-sm border  bg-smoke text-greySmoke text-left">
-                10/01/2023
-              </p>
-            )}
-          </div>
-        </div>
-        <div className="w-fit mx-auto block text-center mt-[2vw]">
-          <p className="text-greySmoke">Case Studies:</p>
-          <CaseStudies />
-        </div>
-        {!pro && <div className="h-10 w-full"></div>}
-        {pro && (
-          <div className="w-full grid grid-cols-2 items-center my-[6vw] px-6">
-            <div className="col-span-1 mr-[2vw] text-center border text-greySmoke">
-              <p>Volatility:</p>
-              <p>{volatility * 100}%</p>
-            </div>
-            <div className="col-span-1 ml-[2vw] text-center border text-greySmoke">
-              <p>Rolling Window:</p>
-              <p>{rollingWindow} days</p>
-            </div>
-          </div>
-        )}
-        <div className="block w-full">
-          <div className="flex items-center justify-center  space-x-2">
-            <div className="w-[2vw] h-1 bg-white"></div>
-            <span className="text-sm">BTC (Raw Price)</span>
-          </div>
-          <div className="flex items-center justify-center space-x-2">
-            <div className="w-[2vw] h-1 bg-carmesi"></div>
-            <p className="text-carmesi text-sm">BTC - Vol Target</p>
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
