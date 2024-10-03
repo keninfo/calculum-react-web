6287f5bb08724d8a379e7b448ef4a20148cce374
Carl Gauss <carlgauss221@gmail.com>
2024-10-02 23:15:57 -0400

    FIXED: added private faucet key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID
-
-export const OWNER_PK = process.env.NEXT_PUBLIC_PRIVATE_KEY
+export const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_WALLET_CONNECT_PROJECT_ID
 
 export const LIVE_PRICES = process.env.NEXT_LIVE_PRICES
+
+export const PRIVATE_KEY = process.env.PRIVATE_KEY
