d779d5e160726050d16a882a0fe58fefb187070c
Carl Gauss <carlgauss221@gmail.com>
2024-10-10 00:46:18 -0400

    FIXED: TESTNET alert

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -35,7 +35,7 @@ const Sidebar = () => {
       <div className="flex justify-around items-center h-fit p-6  | md:hidden">
         <Image src="/bearLogo2.png" width={200} height={80} alt="Picture of the author" className="h-auto w-[50%]" />
         <div className="text-md text-white flex justify-end items-center space-x-2 w-full">
-          <p>DEV BRANCH TEST</p>
+          <p>TESTNET</p>
           <div className="h-3 w-3 rounded-full bg-carmesi animate-pulse"></div>
         </div>
       </div>
