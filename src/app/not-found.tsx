9707bdc7a9b2b61d90f1bc2d5606040e6324cc92
Carl Gauss <carlgauss221@gmail.com>
2024-11-04 18:11:53 -0400

    FIXED: not found page (wip)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,9 +7,11 @@ import { usePathname } from 'next/navigation'
 const Index = () => {
   const pathname = usePathname()
   return (
-    <div className="h-screen">
-      <h1 className="text-red-500">{pathname.toUpperCase().slice(1)}</h1>
-      <p>Page doesn`t exist yet</p>
+    <div className="flex h-screen w-full items-center justify-center bg-smoke">
+      <div>
+        <h1 className="text-center text-yellow-300">404 `{pathname.toUpperCase().slice(1)}` NOT FOUND</h1>
+        <p className="text-center text-white">Page doesn`t exist</p>
+      </div>
     </div>
   )
 }
