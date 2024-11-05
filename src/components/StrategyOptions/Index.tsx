e2293dc86b4366144fbb3270587837db48c2269e
Carl Gauss <carlgauss221@gmail.com>
2024-11-05 00:51:29 -0400

    ADDED: label to strategy options

--- src/components/StrategyOptions/Index.tsx
+++ src/components/StrategyOptions/Index.tsx
@@ -11,11 +11,15 @@ const StrategyOptions = () => {
     <>
       {/* DESKTOP */}
       <Card className="hidden w-full items-center justify-between md:flex">
-        <div className="flex w-full items-center justify-start space-x-10">
+        <div className="flex w-full items-center justify-start space-x-5">
+          <p className="text-grey">STRATEGY: </p>
           <StrategySelect />
           <CoinSelect />
         </div>
-        <NetworkSelect />
+        <div className="flex w-fit items-center justify-start space-x-5">
+          <p className="text-grey">NETWORK: </p>
+          <NetworkSelect />
+        </div>
       </Card>
       {/* MOBILE */}
       <Card className="flex w-full flex-col items-center space-y-4 md:hidden">
