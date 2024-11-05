ebab4d5cc016ef05c7e009eb19de836c95c8631a
Carl Gauss <carlgauss221@gmail.com>
2024-11-04 20:43:26 -0400

    REMOVED: unused OptionsContext

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -2,16 +2,13 @@ import type { ReactNode } from 'react'
 import React from 'react'
 
 import { CoinsProvider } from '@/contexts/CoinsContext'
-import { OptionsProvider } from '@/contexts/OptionsContext'
 import RainbowKit from '@/services/RainbowKitProvider'
 
 const AppProviders = ({ children }: { children: ReactNode }) => {
   return (
-    <OptionsProvider>
-      <CoinsProvider>
-        <RainbowKit>{children}</RainbowKit>
-      </CoinsProvider>
-    </OptionsProvider>
+    <CoinsProvider>
+      <RainbowKit>{children}</RainbowKit>
+    </CoinsProvider>
   )
 }
 
