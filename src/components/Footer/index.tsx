8a36d4e715d714eeaf0ea0a8a5016f10ba8fb7a5
Carl Gauss <carlgauss221@gmail.com>
2024-11-26 20:13:20 -0400

    FIXED: margin bot of footer on mobile

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const index = () => {
   return (
-    <footer className="mb-24 h-fit items-center rounded-lg bg-dark px-10 py-10 md:mb-0 md:grid md:grid-cols-3">
+    <footer className="h-fit items-center rounded-lg bg-dark px-10 py-10 md:mb-0 md:grid md:grid-cols-3">
       <div className="col-span-1 flex items-center justify-center space-x-2 text-grey md:justify-start">
         <p>LinkedIn</p>
         <p>|</p>
