a68d2e86a54188c6ac11af9df2fea99fab4b0b6d
Carl Gauss <carlgauss221@gmail.com>
2024-12-16 16:24:01 -0400

    ADDED: google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,9 +40,8 @@ const RootLayout = ({
     return (
       <html lang="en">
         <body
-          className={`classic overscroll-none bg-cover bg-fixed bg-center ${inter.className} ${
-            pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"
-          }`}
+          className={`classic overscroll-none bg-cover bg-fixed bg-center ${inter.className} ${pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"
+            }`}
         >
           <Watermark />
           <AppProviders>
