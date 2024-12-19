da39fa959d13a826461c970292e4157ee375f6d4
Carl Gauss <carlgauss221@gmail.com>
2024-12-19 10:55:22 -0400

    ADDED: bandit npm and secret key

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,3 +3,5 @@ export const WALLET_CONNECT_PROJECT_ID = process.env.NEXT_PUBLIC_WALLET_CONNECT_
 export const PRIVATE_KEY = process.env.NEXT_PRIVATE_KEY
 
 export const STAGING_PASSWORD = process.env.NEXT_STAGING_PASSWORD
+
+export const BANDIT_KEY = process.env.NEXT_BANDIT_KEY
