205dc58938ab87f7f20e8430b381f1ff6d822daa
Carl Gauss <carlgauss221@gmail.com>
2024-12-23 12:22:12 -0400

    REMOVED: bears from pro toggle

--- src/components/Navbar/ProToggle.tsx
+++ src/components/Navbar/ProToggle.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from 'react'
 
 import { PrimaryButton } from '@/components/common/Buttons'
-import BearAttack from '@/components/common/Icons/BearAttack'
-import BearPassive from '@/components/common/Icons/BearPassive'
 import ToggleSwitch from '@/components/common/ToggleSwitch'
 import { useProStore } from '@/store/useProStore'
 
@@ -22,19 +20,9 @@ const ProToggle = () => {
   return (
     <PrimaryButton
       handleClick={() => setPro(!pro)}
-      className="| flex !w-fit items-center justify-end text-offWhite hover:text-offWhite md:bg-dark md:py-[1vh] md:pl-2"
+      className="flex w-fit items-center justify-start text-offWhite hover:text-offWhite md:bg-dark md:px-4 md:py-[1vh]"
     >
-      {!pro ? (
-        <div className="flex items-center justify-between space-x-2">
-          <ToggleSwitch />
-          <BearPassive className="h-[3vh] -scale-x-100" />
-        </div>
-      ) : (
-        <div className="flex items-center justify-between space-x-2">
-          <ToggleSwitch />
-          <BearAttack className="h-[3vh] -scale-x-100" />
-        </div>
-      )}
+      <ToggleSwitch />
     </PrimaryButton>
   )
 }
