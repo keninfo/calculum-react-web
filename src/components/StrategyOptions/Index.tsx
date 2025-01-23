3fef50e1f669122eced8c8875daaec4a484d467a
nbolinaga <nbolinaga17@gmail.com>
2025-01-23 09:08:23 -0400

    REMOVED: network (chain) selection

--- src/components/StrategyOptions/Index.tsx
+++ src/components/StrategyOptions/Index.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import Card from '@/components/common/Card'
 
 import CoinSelect from './CoinSelect'
-import NetworkSelect from './NetworkSelect'
 import StrategySelect from './StrategySelect'
 
 const StrategyOptions = () => {
@@ -12,20 +11,24 @@ const StrategyOptions = () => {
       {/* DESKTOP */}
       <Card className="z-50 hidden w-full items-center justify-between md:flex">
         <div className="flex w-full items-center justify-start space-x-5">
-          <p className="text-xl text-grey">Strategy: </p>
+          <p className="text-xl text-grey">Product: </p>
           <StrategySelect />
           <CoinSelect />
         </div>
-        <div className="flex w-fit items-center justify-start space-x-5">
+        <div className="text-md flex w-full items-center justify-end space-x-2 text-offWhite">
+          <div className="h-3 w-3 animate-pulse rounded-full bg-primary"></div>
+          <p>TESTNET (Arbitrum Sepolia)</p>
+        </div>
+        {/* <div className="flex w-fit items-center justify-start space-x-5">
           <p className="text-xl text-grey">Network: </p>
           <NetworkSelect />
-        </div>
+        </div> */}
       </Card>
       {/* MOBILE */}
       <Card className="flex w-full flex-col items-center space-y-4 md:hidden">
         <StrategySelect />
         <CoinSelect />
-        <NetworkSelect />
+        {/* <NetworkSelect /> */}
       </Card>
     </>
   )
