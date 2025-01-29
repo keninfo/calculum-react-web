c2431d87a9607d80bc57afc1210338a532738557
Carl Gauss <carlgauss221@gmail.com>
2025-01-29 13:11:59 -0400

    REMOVED: useAnimate

--- src/components/common/Card/index.tsx
+++ src/components/common/Card/index.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useAutoAnimate } from '@formkit/auto-animate/react'
-
 import type { ReactNode } from 'react'
 import React from 'react'
 
@@ -30,9 +28,8 @@ type CardProps = {
  * @returns The `Card` component with customizable content.
  */
 const Card = ({ title, subtitle, children, className }: CardProps) => {
-  const [parent] = useAutoAnimate()
   return (
-    <div className={`h-fit w-fit rounded-lg bg-dark p-5 transition ease-in-out md:p-6 ${className}`} ref={parent}>
+    <div className={`h-fit w-fit rounded-lg bg-dark p-5 transition ease-in-out md:p-6 ${className}`}>
       {(title || subtitle) && (
         <div className="mb-[2vh] flex h-fit items-center justify-between pb-2">
           <h4 className="w-full text-center text-2xl md:text-left">{title}</h4>
