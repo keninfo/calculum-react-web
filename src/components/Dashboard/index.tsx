8c8e2317152e7fca84dbc971d4776595b5d5f39b
Carl Gauss <carlgauss221@gmail.com>
2025-01-31 02:21:56 -0400

    ADDED: opening questions pop-up

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -10,7 +10,10 @@ import { useAccount } from 'wagmi'
 
 import ChartsContainer from '@/components/ChartsContainer/Index'
 import Help from '@/components/Help'
+import MarketTransactions from '@/components/MarketTransactions'
 import Positions from '@/components/Positions'
+import ProductMetrics from '@/components/ProductMetrics'
+import MomentumMetrics from '@/components/ProductMetrics/MomentumMetrics'
 import StrategyInfoTitle from '@/components/StrategyInfoTitle'
 import StrategyOptions from '@/components/StrategyOptions/Index'
 import TradeBox from '@/components/TradeBox'
@@ -20,9 +23,14 @@ import TVAttribution from '@/components/common/TVAttribution'
 import { useProStore } from '@/store/useProStore'
 import { useStrategyStore } from '@/store/useStrategyStore'
 
-import MarketTransactions from '../MarketTransactions'
-import ProductMetrics from '../ProductMetrics'
-import MomentumMetrics from '../ProductMetrics/MomentumMetrics'
+import Modal from '../common/Modal'
+
+const options = [
+  `To accumulate more of my token with no leverage`,
+  `For tax-efficient profit taking (automated)`,
+  ` For a smoother crypto experience`,
+  `For responsible FOMO`,
+]
 
 const Dashboard = () => {
   const { pro, setPro } = useProStore()
@@ -30,6 +38,12 @@ const Dashboard = () => {
   const [selected, setSelected] = useState<number>(1)
   const { isConnected } = useAccount()
   const [ref, { height }] = useMeasure()
+  const [modal, setModal] = useState<boolean>(false)
+  const [shuffledOptions, setShuffledOptions] = useState<string[]>([])
+
+  useEffect(() => {
+    setShuffledOptions([...options].sort(() => Math.random() - 0.5))
+  }, []) // Re-randomize only when options change
 
   useEffect(() => {
     const storedStrategy = localStorage.getItem('strategy')
@@ -46,6 +60,7 @@ const Dashboard = () => {
   useEffect(() => {
     localStorage.setItem('strategy', JSON.stringify(strategy))
     localStorage.setItem('coin', JSON.stringify(coin))
+    setModal(true)
   }, [coin, pro, strategy])
 
   useEffect(() => {
@@ -63,6 +78,27 @@ const Dashboard = () => {
   return (
     <>
       {/* DESKTOP */}
+      {modal && (
+        <Modal onClose={() => setModal(false)} closeMessage="SKIP">
+          <h3 className="mt-14 text-center text-2xl text-robin">{`What's the main reason for your visit, anon?`}</h3>
+          <ul className="grid w-full grid-cols-4 gap-10 px-20 pb-20 pt-10">
+            {shuffledOptions.map((text, index) => (
+              <div
+                key={index}
+                className="relative flex cursor-pointer items-center justify-center rounded-lg hover:bg-payne"
+                onClick={() => setModal(false)}
+              >
+                <div className="absolute left-0 top-0 h-4 w-4 rounded-sm border-l-2 border-t-2 border-[#c1ea60]"></div>
+                <div className="absolute right-0 top-0 h-4 w-4 rounded-sm border-r-2 border-t-2 border-[#c1ea60]"></div>
+                <div className="absolute bottom-0 left-0 h-4 w-4 rounded-sm border-b-2 border-l-2 border-[#c1ea60]"></div>
+                <div className="absolute bottom-0 right-0 h-4 w-4 rounded-sm border-b-2 border-r-2 border-[#c1ea60]"></div>
+
+                <h3 className="p-6 text-center text-[3vw] text-white md:px-4 md:text-[16px]">{text}</h3>
+              </div>
+            ))}
+          </ul>
+        </Modal>
+      )}
       <div className={`hidden grid-cols-11 gap-4 md:grid`}>
         <div
           className={`z-40 col-span-11 -my-4 flex flex-col bg-cover bg-fixed bg-center ${pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"} md:sticky md:top-0`}
