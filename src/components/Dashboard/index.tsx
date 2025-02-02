70ec2360fb123a3822124653acfee77ada806b80
Carl Gauss <carlgauss221@gmail.com>
2025-02-02 11:22:14 -0400

    FIXED: strategy info order and navbar spacing

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,7 +1,5 @@
 'use client'
 
-import { useMeasure } from '@uidotdev/usehooks'
-
 import React, { useEffect, useMemo, useState } from 'react'
 
 import dynamic from 'next/dynamic'
@@ -15,11 +13,11 @@ import Positions from '@/components/Positions'
 import ProductMetrics from '@/components/ProductMetrics'
 import MomentumMetrics from '@/components/ProductMetrics/MomentumMetrics'
 import StrategyInfoTitle from '@/components/StrategyInfoTitle'
-import StrategyOptions from '@/components/StrategyOptions/Index'
 import TradeBox from '@/components/TradeBox'
 import Transactions from '@/components/Transactions'
 import Card from '@/components/common/Card'
 import TVAttribution from '@/components/common/TVAttribution'
+import { useNavbarStore } from '@/store/useNavbarStore'
 import { useProStore } from '@/store/useProStore'
 import { useStrategyStore } from '@/store/useStrategyStore'
 
@@ -30,7 +28,7 @@ const Dashboard = () => {
   const { coin, setCoin, strategy, setStrategy } = useStrategyStore()
   const [selected, setSelected] = useState<number>(1)
   const { isConnected } = useAccount()
-  const [ref, { height }] = useMeasure()
+  const { navbarHeight } = useNavbarStore()
   const [modal, setModal] = useState<boolean>(false)
 
   useEffect(() => {
@@ -74,30 +72,18 @@ const Dashboard = () => {
     <>
       {/* DESKTOP */}
       {modal && <InitialPopup setModal={setModal} />}
-      <div className={`hidden grid-cols-11 gap-4 md:grid`}>
-        <div
-          className={`z-40 col-span-11 -my-4 flex flex-col bg-cover bg-fixed bg-center ${pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"} md:sticky md:top-0`}
-          ref={ref}
-        >
-          <div
-            className={`h-[1rem] w-full bg-cover bg-fixed bg-center ${pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"}`}
-          ></div>
-          <StrategyOptions />
-          <div
-            className={`h-[1rem] w-full bg-cover bg-fixed bg-center ${pro ? "bg-[url('/bgPro.png')]" : "bg-[url('/bg.png')]"}`}
-          ></div>
-        </div>
-        <div className={`col-span-11 flex flex-col`}>
+      <div className={`hidden grid-cols-11 gap-4 md:grid`} style={{ marginTop: navbarHeight }}>
+        <div className={`col-span-11 flex w-full flex-col`}>
           <StrategyInfoTitle />
         </div>
-        <div className={`flex flex-col ${coin !== 'BTC' ? 'col-span-11' : 'col-span-8'}`}>
+        <div className={`flex flex-col ${coin !== 'BTC' || !isConnected ? 'col-span-11' : 'col-span-8'}`}>
           {strategy == 'Momentum' && <TVChartContainer />}
           {strategy == 'Smoothcoin' && <ChartsContainer />}
           <TVAttribution />
           {isConnected && coin == 'BTC' && (
             <Card className="mt-4 h-full min-h-fit w-full">
-              <ul className="grid grid-cols-4 border-b border-payne pt-2">
-                <li className="col-span-1 flex justify-start text-lg">
+              <ul className="flex items-center justify-start gap-10 border-b border-payne pt-2">
+                <li className="text-lg">
                   <button
                     onClick={() => setSelected(0)}
                     className={`pb-6 ${selected === 0 ? 'border-b-2 border-primary text-primary' : ''}`}
@@ -105,28 +91,28 @@ const Dashboard = () => {
                     Metrics
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-start text-lg">
+                <li className="text-lg">
                   <button
                     onClick={() => setSelected(1)}
                     className={`pb-6 ${selected === 1 ? 'border-b-2 border-primary text-primary' : ''}`}
                   >
-                    Market Transactions
+                    Transactions
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-center text-lg">
+                <li className="text-lg">
                   <button
                     onClick={() => setSelected(2)}
                     className={`pb-6 ${selected === 2 ? 'border-b-2 border-primary text-primary' : ''}`}
                   >
-                    My Positions
+                    Positions
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-end text-lg">
+                <li className="text-lg">
                   <button
                     onClick={() => setSelected(3)}
                     className={`pb-6 ${selected === 3 ? 'border-b-2 border-primary text-primary' : ''}`}
                   >
-                    My Transaction History
+                    Transaction History
                   </button>
                 </li>
               </ul>
@@ -148,12 +134,12 @@ const Dashboard = () => {
                     Metrics
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-end text-lg">
+                <li className="col-span-1 flex justify-start text-lg">
                   <button
                     onClick={() => setSelected(1)}
                     className={`pb-6 ${selected === 1 ? 'border-b-2 border-primary text-primary' : ''}`}
                   >
-                    Market Transactions
+                    Transactions
                   </button>
                 </li>
               </ul>
@@ -163,9 +149,9 @@ const Dashboard = () => {
             </Card>
           )}
         </div>
-        {coin == 'BTC' && (
+        {coin == 'BTC' && isConnected && (
           <div className="relative col-span-3 flex h-full flex-col gap-4">
-            <div className="sticky top-0 space-y-4" style={{ top: height || 0 }}>
+            <div className="sticky top-0 space-y-4" style={{ top: navbarHeight || 0 }}>
               <TradeBox />
               <Help />
             </div>
@@ -174,55 +160,58 @@ const Dashboard = () => {
       </div>
 
       {/* MOBILE */}
-      <div className="-mt-5 block w-screen space-y-5 px-5 md:hidden">
-        <StrategyOptions />
+      <div className="-mt-5 block w-screen space-y-5 px-5 md:hidden" style={{ marginTop: navbarHeight }}>
         <StrategyInfoTitle />
-
         {strategy == 'Momentum' && <TVChartContainer />}
         {strategy == 'Smoothcoin' && <ChartsContainer />}
         <TVAttribution />
-        <Help />
-        <TradeBox />
+        {isConnected && (
+          <>
+            <Help />
+            <TradeBox />
+          </>
+        )}
         <div className={`flex flex-col ${coin !== 'BTC' ? 'col-span-11' : 'col-span-8'}`}>
           {isConnected && coin == 'BTC' && (
             <Card className="mt-4 h-full min-h-fit w-full">
               <ul className="grid grid-cols-1 border-b border-payne pt-2 md:grid-cols-4">
-                <li className="col-span-1 flex justify-center text-lg md:justify-start">
+                <li className="col-span-1 flex justify-start text-lg">
                   <button
                     onClick={() => setSelected(0)}
                     className={`pb-6 ${selected === 0 ? 'border-primary text-primary md:border-b-2' : ''}`}
                   >
-                    Market Transactions
+                    Metrics
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-center text-lg">
+                <li className="col-span-1 flex justify-start text-lg md:justify-start">
                   <button
                     onClick={() => setSelected(1)}
                     className={`pb-6 ${selected === 1 ? 'border-primary text-primary md:border-b-2' : ''}`}
                   >
-                    Metrics
+                    Transactions
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-center text-lg">
+
+                <li className="col-span-1 flex justify-start text-lg">
                   <button
                     onClick={() => setSelected(2)}
                     className={`pb-6 ${selected === 2 ? 'border-primary text-primary md:border-b-2' : ''}`}
                   >
-                    My Positions
+                    Positions
                   </button>
                 </li>
-                <li className="col-span-1 flex justify-center text-lg md:justify-end">
+                <li className="col-span-1 flex justify-start text-lg md:justify-end">
                   <button
                     onClick={() => setSelected(3)}
                     className={`pb-6 ${selected === 3 ? 'border-primary text-primary md:border-b-2' : ''}`}
                   >
-                    My Transaction History
+                    Transaction History
                   </button>
                 </li>
               </ul>
-              {selected == 0 && <MarketTransactions />}
-              {selected == 1 && strategy == 'Momentum' && <MomentumMetrics />}
-              {selected == 1 && strategy == 'Smoothcoin' && <ProductMetrics />}
+              {selected == 0 && strategy == 'Momentum' && <MomentumMetrics />}
+              {selected == 0 && strategy == 'Smoothcoin' && <ProductMetrics />}
+              {selected == 1 && <MarketTransactions />}
               {selected == 2 && <Positions />}
               {selected == 3 && <Transactions />}
             </Card>
