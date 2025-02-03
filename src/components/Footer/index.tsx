ad6ae80d356f8be0f24c56b48b898060753038b7
Carl Gauss <carlgauss221@gmail.com>
2025-02-03 17:53:26 -0400

    FIXED: footer logo image size

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -16,7 +16,7 @@ const index = () => {
         </a>
       </div>
       <div className="col-span-1 my-5 flex h-full w-auto items-center justify-center px-20 md:my-0">
-        <Image src="/HODL.png" width={500} height={1000} alt="image" className="h-10 w-auto" />
+        <Image src="/HODL.png" width={500} height={1000} alt="image" className="h-auto w-full md:h-10 md:w-auto" />
       </div>
 
       <p className="col-span-1 text-center text-grey md:text-right">HODL Protocol @ 2025. All Rights Reserved</p>
