e6d231f7d9e664286feffeab4dce313cb81308b3
Carl Gauss <carlgauss221@gmail.com>
2025-02-04 16:20:09 -0400

    FIXED: products page

--- src/components/Home/WorkingContracts.tsx
+++ src/components/Home/WorkingContracts.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 
 import { useRouter } from 'next/navigation'
 
+import type { Abi, Hash } from 'viem'
+
+import { useAccount } from 'wagmi'
+
 import Card from '@/components/common/Card'
 import { contractIndex } from '@/contracts/contractIndex'
+import ContractReads from '@/hooks/useContractReads'
 import { useStrategyStore } from '@/store/useStrategyStore'
+import { formatShares } from '@/utils/formatters'
 
-const WorkingContracts = () => {
+type responseData = [number, bigint, bigint, bigint]
+
+const WorkingContracts = ({ myPositions = false }: { myPositions?: boolean }) => {
   const { setStrategy, setCoin } = useStrategyStore()
   const router = useRouter()
+  const { isConnected, address } = useAccount()
 
   const goTo = (strategy: string, coin: string) => {
     setStrategy(strategy)
@@ -18,42 +27,153 @@ const WorkingContracts = () => {
     router.push('/dashboard')
   }
 
-  return (
-    <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
-      {contractIndex
-        .filter((contract) => contract.isWorking)
-        .map((contract, index) => (
-          <Card className="w-full" key={index}>
-            <div className="items-start justify-start md:flex md:space-x-5">
-              <div className="w-full">
-                <h2 className="w-full text-nowrap text-center text-[7vw] font-medium md:text-left md:text-4xl">
-                  {contract.strategy} {contract.coin}
-                </h2>
-                <p className="w-full text-nowrap text-center text-xs text-citron md:text-left md:text-sm">
-                  Arbitrum Sepolia <b className="font-light text-offWhite">- Testnet</b>
-                </p>
-                <p className="mt-5 w-full text-center text-grey md:text-left md:text-lg">{contract.info}</p>
+  // Prepare all contract reads upfront
+  const contractReadResults = contractIndex.map((contract) => {
+    if (!contract.isWorking) {
+      return { ...contract, finalAmount: '-', isPlaceholder: false }
+    }
+    const { Deposits, BalanceShares } = ContractReads(contract.address as Hash, contract.abi as Abi)
+    const depositsData = Deposits(address).data as responseData | undefined
+    const balanceSharesResult = BalanceShares(address).data as bigint | undefined
 
-                <div className="mt-5 md:flex md:w-full md:justify-between">
-                  <button
-                    className="mt-5 w-full rounded-lg bg-primary px-5 py-2 md:mt-0 md:w-fit"
-                    onClick={() => goTo(contract.strategy, contract.coin)}
-                  >
-                    <p className="text-dark">Start Trading</p>
-                  </button>
+    let calculatedShares = depositsData ? depositsData[2] : BigInt(0)
+    if (calculatedShares <= 0) {
+      calculatedShares = balanceSharesResult || BigInt(0)
+    }
+
+    const finalAmount = isConnected ? parseFloat(formatShares(calculatedShares)).toLocaleString() : '-'
+
+    return { ...contract, finalAmount, isPlaceholder: false }
+  })
+
+  let filtered = contractReadResults.filter((contract) => Number(contract.finalAmount) != 0 && contract.isWorking)
+
+  if (!myPositions) {
+    filtered = contractReadResults.filter(
+      (contract) => Number(contract.finalAmount) == 0 || contract.finalAmount == '-',
+    )
+  }
+
+  const placeholdersNeeded = (4 - (filtered.length % 4)) % 4
+
+  for (let i = 0; i < placeholdersNeeded; i++) {
+    filtered.push({
+      isPlaceholder: true,
+      finalAmount: '',
+      isWorking: false,
+      strategy: '',
+      coin: '',
+      abi: [],
+      address: '',
+      chainId: '',
+      chain: '',
+      symbol: '',
+      icon: '',
+      info: '',
+    })
+  }
+
+  if (filtered.length == 0) {
+    return (
+      <p className="bg-black py-5 text-center text-primary md:ml-6 md:text-left">{`You don't have any open positions`}</p>
+    )
+  }
+
+  return (
+    <>
+      {filtered.map((contract, index) => (
+        <>
+          {contract.isWorking && (
+            <Card
+              key={index}
+              className={`h-full w-full !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-45' : 'opacity-90'}`}
+            >
+              {!contract.isPlaceholder ? (
+                <div
+                  className={`grid w-full cursor-pointer grid-cols-2 rounded-md border border-dark hover:border-offWhite`}
+                  onClick={() => goTo(contract.strategy, contract.coin)}
+                >
+                  <div className="col-span-1 p-4">
+                    <img src={contract.icon} width={50} height={50} alt={contract.symbol} className="h-6 w-6" />
+                    <div className="space-y-2 pt-2">
+                      <div>
+                        <p className="text-xs text-grey">Token</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.symbol}</h2>
+                      </div>
+                      <div>
+                        <p className="text-xs text-grey">Strategy</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.strategy}</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-span-1 p-4">
+                    <div className="h-6 w-6"></div>
+                    <div className="space-y-2 pt-2">
+                      <div>
+                        <p className="text-right text-xs text-grey">Network</p>
+                        <h2 className="w-full text-nowrap pt-1 text-right text-sm font-medium">{contract.chain}</h2>
+                      </div>
+                      <div>
+                        <p className="text-right text-xs text-grey">Wallet Balance</p>
+                        <h2 className="w-full text-nowrap text-right text-lg font-medium">{contract.finalAmount}</h2>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-              <img
-                src={contract.icon}
-                width={50}
-                height={50}
-                alt={contract.symbol}
-                className="mx-auto mt-4 rounded-full md:mx-0 md:mt-1"
-              />
-            </div>
-          </Card>
-        ))}
-    </div>
+              ) : (
+                <div className="h-full w-full border-spacing-5 rounded-md border border-dashed">
+                  <p></p>
+                </div>
+              )}
+            </Card>
+          )}
+          {!contract.isWorking && (
+            <Card
+              key={index}
+              className={`h-full w-full bg-eerie !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-45' : 'opacity-90'}`}
+            >
+              {!contract.isPlaceholder ? (
+                <div
+                  className={`grid h-full w-full cursor-pointer grid-cols-2 rounded-md border border-dark hover:border-primary`}
+                  onClick={() => goTo(contract.strategy, contract.coin)}
+                >
+                  <div className="col-span-1 p-4">
+                    <img src={contract.icon} width={50} height={50} alt={contract.symbol} className="h-6 w-6" />
+                    <div className="space-y-2 pt-2">
+                      <div>
+                        <p className="text-xs text-grey">Token</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.symbol}</h2>
+                      </div>
+                      <div>
+                        <p className="text-xs text-grey">Strategy</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.strategy}</h2>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-span-1 p-4">
+                    <div className="h-6 w-6"></div>
+                    <div className="space-y-2 pt-2">
+                      <div>
+                        <p className="text-right text-xs text-grey">Network</p>
+                        <h2 className="w-full text-nowrap text-right text-lg font-medium">{contract.chain}</h2>
+                      </div>
+                      <div>
+                        <p className="text-right text-xs text-grey">Opening</p>
+                        <h2 className="w-full text-nowrap text-right text-lg font-medium">Q2 2025</h2>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div className="h-full w-full border-spacing-5 rounded-md border border-dashed">
+                  <p></p>
+                </div>
+              )}
+            </Card>
+          )}
+        </>
+      ))}
+    </>
   )
 }
 
--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -4,31 +4,70 @@ import React from 'react'
 
 import Image from 'next/image'
 
-import ComingSoonContracts from './ComingSoonContracts'
+import { useAccount } from 'wagmi'
+
+import Card from '@/components/common/Card'
+import Select from '@/components/common/Select'
+import { useNavbarStore } from '@/store/useNavbarStore'
+
 import WorkingContracts from './WorkingContracts'
 
 const Index = () => {
+  const { navbarHeight } = useNavbarStore()
+  const { isConnected } = useAccount()
   return (
-    <>
-      <div className="mx-auto mb-20 md:w-[60%]">
-        <h1 className="mb-5 text-center text-4xl">Welcome to</h1>
-        <Image src="/hodllogo.svg" width={50} height={100} alt="image" className="h-auto w-full" />
+    <div className="relative h-full min-h-screen overflow-hidden px-10 md:px-0" style={{ marginTop: navbarHeight }}>
+      <div className="fixed left-1/2 top-full -z-10 hidden h-screen w-full -translate-x-1/2 -translate-y-1/2 items-end justify-center md:flex">
+        <Image
+          src={'/moon.png'}
+          width={1000}
+          height={1000}
+          alt="photo of the earth viewed from space / black and white"
+          className="h-auto w-full md:h-[100vh] md:w-auto"
+          style={{
+            animation: 'rotate 1000s linear infinite',
+          }}
+        />
       </div>
-      <div className="my-10 items-center justify-between text-center md:flex md:text-left">
-        <h3 className="text-3xl">Our Products</h3>
-        <div className="hidden rounded-lg bg-dark px-5 py-3 md:block">
-          <ul className="flex items-center justify-center space-x-5">
-            <li className={`text-citron`}>All</li>
-            <li>|</li>
-            <li>Arbitrum Sepolia</li>
-          </ul>
+      {isConnected && (
+        <>
+          <Card className="mb-4 w-full items-center justify-between !py-1 md:flex">
+            <h3 className="text-center text-2xl md:text-left md:text-xl">My Positions</h3>
+          </Card>
+          <div className="mb-4 grid w-full grid-cols-1 gap-3 md:grid-cols-4">
+            <WorkingContracts myPositions />
+          </div>
+        </>
+      )}
+      <Card className="mb-4 w-full items-center justify-between bg-eerie !py-1 md:flex">
+        <h3 className="text-center text-2xl md:text-left md:text-xl">Our Products</h3>
+        <div className="flex items-center justify-center space-x-6 md:justify-end">
+          <div className="flex items-center justify-center space-x-2">
+            <label>Network:</label>
+            <Select
+              handleChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
+                throw new Error(`Function not implemented: ${e}`)
+              }}
+              value={'Network'}
+              options={['All']}
+            ></Select>
+          </div>
+          <div className="flex items-center justify-center space-x-2">
+            <label>Token:</label>
+            <Select
+              handleChange={function (e: React.ChangeEvent<HTMLSelectElement>): void {
+                throw new Error(`Function not implemented: ${e}`)
+              }}
+              value={'Token'}
+              options={['All']}
+            ></Select>
+          </div>
         </div>
+      </Card>
+      <div className="mb-4 grid grid-cols-1 gap-3 md:grid-cols-4">
+        <WorkingContracts />
       </div>
-
-      <WorkingContracts />
-      <h4 className="my-10 text-3xl text-citron">Coming Soon...</h4>
-      <ComingSoonContracts />
-    </>
+    </div>
   )
 }
 
