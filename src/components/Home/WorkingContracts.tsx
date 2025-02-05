402c19b9e266ae700ee24efb1be85caedb3ce165
Carl Gauss <carlgauss221@gmail.com>
2025-02-05 14:58:58 -0400

    FIXED: product card

--- src/components/Home/WorkingContracts.tsx
+++ src/components/Home/WorkingContracts.tsx
@@ -12,7 +12,7 @@ import ContractReads from '@/hooks/useContractReads'
 import { useStrategyStore } from '@/store/useStrategyStore'
 import { formatShares } from '@/utils/formatters'
 
-import CryptoIcon from '../common/CryptoIcon'
+import { PrimaryButton } from '../common/Buttons'
 
 type responseData = [number, bigint, bigint, bigint]
 
@@ -88,48 +88,63 @@ const WorkingContracts = ({ myPositions = false }: { myPositions?: boolean }) =>
           {contract.isWorking && (
             <Card
               key={index}
-              className={`h-full w-full bg-eerie !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-45' : 'opacity-90 hover:opacity-100'}`}
+              className={`h-full w-full bg-eerie !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-70' : 'opacity-100'}`}
             >
               {!contract.isPlaceholder ? (
-                <div
-                  className={`relative grid w-full cursor-pointer grid-cols-2 rounded-md border border-offWhite hover:border-primary`}
-                  onClick={() => goTo(contract.strategy, contract.coin)}
-                >
+                <div className={`relative grid w-full grid-cols-2 rounded-md border border-offWhite`}>
                   <div className="col-span-1 p-4">
                     <div className="flex items-center justify-start space-x-2">
                       <img src={contract.icon} width={50} height={50} alt={contract.symbol} className="h-6 w-6" />
-                      <CryptoIcon coin="USDC" className="h-[20px]" />
                     </div>
-                    <div className="space-y-2 pt-2">
+                    <div className="space-y-6 pt-2">
                       <div>
                         <p className="text-xs text-grey">Token</p>
-                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.symbol}</h2>
+                        <h2 className="w-full text-nowrap text-left text-2xl font-medium">{contract.symbol}</h2>
                       </div>
                       <div>
-                        <p className="text-xs text-grey">Strategy</p>
-                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.strategy}</h2>
+                        <p className="text-left text-xs text-grey">Token Balance</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.finalAmount}</h2>
                       </div>
                     </div>
                   </div>
                   <div className="col-span-1 p-4">
-                    <div className="h-6 w-6"></div>
+                    <div className="flex items-center justify-end space-x-2">
+                      <img src={'/arb-logo.png'} width={50} height={50} alt={contract.symbol} className="h-6 w-6" />
+                    </div>
                     <div className="space-y-2 pt-2">
                       <div>
-                        <p className="text-right text-xs text-grey">Network</p>
-                        <h2 className="w-full text-nowrap py-1 text-right text-sm font-medium text-primary">
-                          {contract.chain}
-                        </h2>
-                      </div>
-                      <div>
-                        <p className="text-right text-xs text-grey">Wallet Balance</p>
-                        <h2 className="w-full text-nowrap text-right text-lg font-medium">{contract.finalAmount}</h2>
+                        <p className="text-right text-xs text-grey">Strategy</p>
+                        <h2 className="-mb-1 w-full text-nowrap text-right text-lg font-medium">{contract.strategy}</h2>
+                        <div className="flex justify-end">
+                          <a
+                            className="text-xs font-light text-primary underline"
+                            href={
+                              contract.strategy == 'Momentum'
+                                ? 'https://hodlprotocol.gitbook.io/hodl-academy/understanding-momentum/what-is-momentum'
+                                : 'https://hodlprotocol.gitbook.io/hodl-academy/understanding-smoothcoins/what-are-smoothcoins'
+                            }
+                            target="_blank"
+                          >
+                            Learn more
+                          </a>
+                        </div>
                       </div>
                     </div>
+                    <div className="mt-6 pl-6">
+                      <PrimaryButton
+                        handleClick={() => goTo(contract.strategy, contract.coin)}
+                        className="!rounded-md !py-1 text-sm"
+                      >
+                        <p className="flex w-full items-center justify-center space-x-6">
+                          {Number(contract.finalAmount) > 0 || contract.finalAmount == '-' ? 'View' : 'Deposit'}
+                          {/* <FontAwesomeIcon icon={['fas', 'arrow-right' as IconName]} className='-rotate-45 ml-1' /> */}
+                        </p>
+                      </PrimaryButton>
+                    </div>
                   </div>
-                  <p className="absolute right-4 top-4 text-xs text-payne">Click to start trading</p>
                 </div>
               ) : (
-                <div className="h-full w-full border-spacing-5 rounded-md border border-dashed">
+                <div className="hidden h-full w-full border-spacing-5 rounded-md border border-dashed md:block">
                   <p></p>
                 </div>
               )}
@@ -138,26 +153,22 @@ const WorkingContracts = ({ myPositions = false }: { myPositions?: boolean }) =>
           {!contract.isWorking && (
             <Card
               key={index}
-              className={`h-full w-full bg-eerie !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-70' : 'opacity-70 hover:opacity-100'}`}
+              className={`h-full w-full bg-black !p-0 ${contract.isPlaceholder ? 'bg-transparent opacity-70' : 'opacity-90'}`}
             >
               {!contract.isPlaceholder ? (
-                <div
-                  className={`relative grid h-full w-full cursor-pointer grid-cols-2 rounded-md border border-offWhite hover:border-primary`}
-                  onClick={() => goTo(contract.strategy, contract.coin)}
-                >
+                <div className={`relative grid h-full w-full grid-cols-2 rounded-md border border-black`}>
                   <div className="col-span-1 p-4">
                     <div className="flex items-center justify-start space-x-2">
                       <img src={contract.icon} width={50} height={50} alt={contract.symbol} className="h-6 w-6" />
-                      <CryptoIcon coin="USDC" className="h-[20px]" />
                     </div>
-                    <div className="space-y-2 pt-2">
+                    <div className="space-y-5 pt-2">
                       <div>
                         <p className="text-xs text-grey">Token</p>
-                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.symbol}</h2>
+                        <h2 className="w-full text-nowrap text-left text-2xl font-medium">{contract.symbol}</h2>
                       </div>
                       <div>
-                        <p className="text-xs text-grey">Strategy</p>
-                        <h2 className="w-full text-nowrap text-left text-lg font-medium">{contract.strategy}</h2>
+                        <p className="text-left text-xs text-grey">Launching</p>
+                        <h2 className="w-full text-nowrap text-left text-lg font-medium">Q2 2025</h2>
                       </div>
                     </div>
                   </div>
@@ -165,21 +176,38 @@ const WorkingContracts = ({ myPositions = false }: { myPositions?: boolean }) =>
                     <div className="h-6 w-6"></div>
                     <div className="space-y-2 pt-2">
                       <div>
-                        <p className="text-right text-xs text-grey">Network</p>
-                        <h2 className="w-full text-nowrap pt-1 text-right text-sm font-medium text-robin">
-                          {contract.chain}
-                        </h2>
-                      </div>
-                      <div>
-                        <p className="text-right text-xs text-grey">Opening</p>
-                        <h2 className="w-full text-nowrap text-right text-lg font-medium">Q2 2025</h2>
+                        <p className="text-right text-xs text-grey">Strategy</p>
+                        <h2 className="-mb-1 w-full text-nowrap text-right text-lg font-medium">{contract.strategy}</h2>
+                        <div className="flex justify-end">
+                          <a
+                            className="text-xs font-light text-primary underline"
+                            href={
+                              contract.strategy == 'Momentum'
+                                ? 'https://hodlprotocol.gitbook.io/hodl-academy/understanding-momentum/what-is-momentum'
+                                : 'https://hodlprotocol.gitbook.io/hodl-academy/understanding-smoothcoins/what-are-smoothcoins'
+                            }
+                            target="_blank"
+                          >
+                            Learn more
+                          </a>
+                        </div>
                       </div>
                     </div>
+                    <div className="mt-6 pl-6">
+                      <PrimaryButton
+                        handleClick={() => goTo(contract.strategy, contract.coin)}
+                        className="!rounded-md bg-robin !py-1 text-sm"
+                      >
+                        <p className="flex w-full items-center justify-center space-x-6">
+                          Preview
+                          {/* <FontAwesomeIcon icon={['fas', 'arrow-right' as IconName]} className='-rotate-45 ml-1' /> */}
+                        </p>
+                      </PrimaryButton>
+                    </div>
                   </div>
-                  <p className="absolute right-4 top-4 text-xs text-payne">Click to learn more</p>
                 </div>
               ) : (
-                <div className="h-full w-full border-spacing-5 rounded-md border border-dashed">
+                <div className="hidden h-full w-full border-spacing-5 rounded-md border border-dashed md:block">
                   <p></p>
                 </div>
               )}
