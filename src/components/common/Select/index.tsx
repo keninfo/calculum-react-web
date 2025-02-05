402de51defd2b9490f70852dbb68ffe0da55b864
Carl Gauss <carlgauss221@gmail.com>
2025-02-05 14:57:28 -0400

    FIXED: select color

--- src/components/common/Select/index.tsx
+++ src/components/common/Select/index.tsx
@@ -42,7 +42,7 @@ const Select = ({ handleChange, value, options, className = '', disabled = false
   return (
     <div className="relative w-fit">
       <select
-        className={`text-md h-fit w-full cursor-pointer bg-transparent py-1 text-left md:w-max ${disabled ? 'pr-0' : 'pr-8 text-citron'} ${className}`}
+        className={`text-md h-fit w-full cursor-pointer bg-transparent py-1 text-left md:w-max ${disabled ? 'pr-0' : 'pr-8 text-offWhite'} ${className}`}
         id="cryptoCoin"
         onChange={handleChange}
         value={value}
