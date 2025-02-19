7696506737fd1e10f86a568e35c3ba2489736339
Carl Gauss <carlgauss221@gmail.com>
2025-02-19 10:12:35 -0500

    ADDED: 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,39 @@
 
 import React from 'react'
 
+import Image from 'next/image'
 import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 
-const Index = () => {
+import { PrimaryButton } from '@/components/common/Buttons'
+
+const NotFoundPage = () => {
   const pathname = usePathname()
+  const router = useRouter()
+
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-eerie">
-      <div>
-        <h1 className="text-center text-citron">404 `{pathname.toUpperCase().slice(1)}` NOT FOUND</h1>
-        <p className="text-center text-offWhite">Page doesn`t exist</p>
+    <div className="relative flex h-screen w-full flex-col items-center justify-center text-offWhite">
+      <div className="absolute left-0 top-0 -z-10 flex h-full w-full -translate-y-20 items-center justify-start opacity-80">
+        <Image
+          src={'/bear.png'}
+          width={4000}
+          height={4000}
+          alt="background picture of blueprints"
+          priority
+          className="h-1/4 w-auto translate-x-full"
+        />
       </div>
+      <h1 className="text-6xl font-bold text-citron">404</h1>
+      <h2 className="mt-2 text-2xl font-semibold">`{pathname.toUpperCase().slice(1)}` NOT FOUND</h2>
+      <p className="mt-4 text-lg text-gray-400">The page you are looking for doesn’t exist or has been moved.</p>
+      <PrimaryButton
+        className="mt-6 w-1/4 rounded-lg bg-primary px-6 py-2 text-dark hover:bg-robin"
+        handleClick={() => router.push('/')}
+      >
+        Go Home
+      </PrimaryButton>
     </div>
   )
 }
 
-export default Index
+export default NotFoundPage
