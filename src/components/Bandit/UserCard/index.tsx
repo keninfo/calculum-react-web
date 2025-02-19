cbfca05d432f4a4845568692acd18248bc8c8596
nbolinaga <nbolinaga17@gmail.com>
2025-02-19 00:19:07 -0500

    REMOVED: rocket from leaderboard

--- src/components/Bandit/UserCard/index.tsx
+++ src/components/Bandit/UserCard/index.tsx
@@ -7,8 +7,6 @@ import { useAccount } from 'wagmi'
 
 import Card from '@/components/common/Card'
 
-const MAX_XP = 210 // Define the maximum XP corresponding to 100%
-
 const UserCard = ({ campaignId }: { campaignId: number }) => {
   const { isConnected } = useAccount()
   return (
@@ -19,11 +17,6 @@ const UserCard = ({ campaignId }: { campaignId: number }) => {
           const { userQuery } = args
           const user = userQuery.data
 
-          let rocketHeight = 0
-          if (user && user.xp) {
-            rocketHeight = Math.min((user.xp / MAX_XP) * 100, 100) // Normalize XP to 100% and cap at 100%
-          }
-
           if ((user?.rank !== 0 || user?.rank == null) && isConnected) {
             return (
               <div className="relative mt-4 h-full">
@@ -33,16 +26,6 @@ const UserCard = ({ campaignId }: { campaignId: number }) => {
                 <p className="text-center">
                   with a total of <b className="text-citron">{user?.xp}</b> Moontonium
                 </p>
-                <p className="absolute bottom-[66%] left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 text-center text-5xl">
-                  🪐
-                </p>
-                <div className="absolute bottom-9 left-1/2 z-10 h-4/6 w-4 -translate-x-1/2 bg-payne"></div>
-                <div
-                  className={`absolute bottom-9 left-1/2 z-40 w-5 -translate-x-1/2 bg-citron`}
-                  style={{ height: rocketHeight + '%' }}
-                >
-                  <p className="absolute -top-5 left-1/2 -translate-x-1/2 text-5xl">🚀</p>
-                </div>
               </div>
             )
           } else if (isConnected) {
