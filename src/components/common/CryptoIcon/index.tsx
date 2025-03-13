1bf59c22a7807e86b91298f874c005a0f3d9a1ea
Carl Gauss <carlgauss221@gmail.com>
2025-03-13 00:54:50 -0400

    ADDED: fallback to crypto icon component

--- src/components/common/CryptoIcon/index.tsx
+++ src/components/common/CryptoIcon/index.tsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-/** * Properties for the `CryptoIcon` component. */
+import Image from 'next/image'
+
+/** Properties for the `CryptoIcon` component. */
 type CryptoIconProps = {
-  /** * The name of the cryptocurrency (e.g., 'BTC', 'ETH'). */
+  /** The name of the cryptocurrency (e.g., 'BTC', 'ETH'). */
   coin: string
-  /** * Optional CSS class for custom styling of the icon. */
+  /** Optional CSS class for custom styling of the icon. */
   className?: string
-  /** * The type of icon (e.g., 'color' or 'mono'). Defaults to 'color'. */
+  /** The type of icon (e.g., 'color' or 'mono'). Defaults to 'color'. */
   type?: string
 }
 
 /**
  * A component to display a cryptocurrency icon.
  *
- * @remarks
- * The component dynamically loads an icon based on the `coin` prop from a CDN and applies the optional `className`.
- *
- * @param coin - The name of the cryptocurrency to display (e.g., 'BTC', 'ETH').
- * @param className - Optional CSS class for custom styling.
- * @param type - The type of icon (e.g., 'color' or 'mono'). Defaults to 'color'.
- * @returns The cryptocurrency icon image element.
+ * If the icon is not found, a generic placeholder is used.
  */
 const CryptoIcon = ({ coin, className, type = 'color' }: CryptoIconProps) => {
+  const baseUrl =
+    'https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/svg'
+
+  // Construct the URL for the coin and the fallback URL
+  const coinIcon = `${baseUrl}/${type}/${coin.toLowerCase()}.svg`
+  const fallbackIcon = `${baseUrl}/${type}/generic.svg`
+
+  const [imgSrc, setImgSrc] = useState(fallbackIcon)
+
+  useEffect(() => {
+    setImgSrc(coinIcon)
+  }, [coin, coinIcon])
+
   return (
-    <img
-      src={`https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@1a63530be6e374711a8554f31b17e4cb92c25fa5/svg/${type}/${coin.toLowerCase()}.svg`}
-      alt="coin icon"
+    <Image
+      src={imgSrc}
+      alt={''}
       className={className}
+      width={32} // Set width to prevent layout shift
+      height={32} // Set height to prevent layout shift
+      onError={() => setImgSrc(fallbackIcon)} // On error, fallback to the generic icon
     />
   )
 }
