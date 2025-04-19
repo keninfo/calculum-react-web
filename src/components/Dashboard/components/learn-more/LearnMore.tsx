8b2ea0c2e0c40ecd57d72525cdae584d35e2cb90
Ruben Padilla <alejandro010492@gmail.com>
2025-04-19 15:37:42 -0400

    fix: learn more links filter by strategy

--- src/components/Dashboard/components/learn-more/LearnMore.tsx
+++ src/components/Dashboard/components/learn-more/LearnMore.tsx
@@ -3,20 +3,24 @@ import type { FC } from 'react'
 import Image from 'next/image'
 
 import Card from '@/components/common/Card'
+import { useStrategyStore } from '@/store/useStrategyStore'
 
 const links = [
   {
     label: 'Step-by-step guides',
+    strategies: ['smoothcoin', 'momentum'],
     path: 'https://docs.hodlprotocol.io/video-tutorial',
   },
   {
     label: 'Momentum strategies',
+    strategies: ['momentum'],
     path: 'https://docs.hodlprotocol.io/hodl-101/what-is-momentum',
   },
-  // {
-  //   label: 'Mantle Network',
-  //   path: 'https://docs.hodlprotocol.io/hodl-101/what-are-smoothcoins',
-  // },
+  {
+    label: 'SmoothCoins strategies',
+    strategies: ['smoothcoin'],
+    path: 'https://docs.hodlprotocol.io/hodl-101/what-are-smoothcoins',
+  },
   // {
   //   label: 'How to connect your wallet',
   //   path: '/#',
@@ -28,20 +32,24 @@ const links = [
 ]
 
 const LearnMore: FC = () => {
+  const { strategy } = useStrategyStore()
+
   return (
     <Card className="flex w-full flex-col space-y-5 bg-[#3B3B3B]">
       <h2 className="text-2xl font-semibold text-[#7F7F7F]">Learn More</h2>
       <ul className="space-y-2">
-        {links.map((item) => (
-          <li key={item.label}>
-            <a href={item.path} target="_blank" rel="noopener">
-              <div className="flex w-full items-center gap-3">
-                <p>{item.label}</p>
-                <Image src="/icons/external_arrow.svg" alt="external link" width={12} height={12} />
-              </div>
-            </a>
-          </li>
-        ))}
+        {links
+          .filter((item) => item.strategies?.includes(strategy.toLowerCase()))
+          .map((item) => (
+            <li key={item.label}>
+              <a href={item.path} target="_blank" rel="noopener">
+                <div className="flex w-full items-center gap-3">
+                  <p>{item.label}</p>
+                  <Image src="/icons/external_arrow.svg" alt="external link" width={12} height={12} />
+                </div>
+              </a>
+            </li>
+          ))}
       </ul>
     </Card>
   )
