b173045e0390c23e7dc856aca133ac8a4552c783
HODL-HZ <huanzi.shao@hodlprotocol.io>
2025-05-22 06:56:31 +0100

    velvet_v1_endpoints_only

--- src/components/user-positions/services/velvet-tvl.service.ts
+++ src/components/user-positions/services/velvet-tvl.service.ts
@@ -6,10 +6,12 @@ import type { Hash } from 'viem'
 import { VELVET_CAPITAL_PORTFOLIO } from '@/shared/constants'
 
 import type { VelvetPortfolioTransactionsResponse, VelvetTVLResponse } from '../types'
+import { VELVET_CAPITAL_INTENTS_BASE_API_URL } from '@/shared/constants'
+
 
 export const getVelvetPortfolioTVLService = async (): Promise<VelvetTVLResponse> => {
   const { data } = await axios.get(
-    `https://intents.velvet.capital/api/v1/portfolio/tvl?portfolios=${VELVET_CAPITAL_PORTFOLIO}&chainID=8453`,
+    `${VELVET_CAPITAL_INTENTS_BASE_API_URL}/v1/portfolio/tvl?portfolios=${VELVET_CAPITAL_PORTFOLIO}&chainID=8453`,
   )
   return data
 }
@@ -17,5 +19,5 @@ export const getVelvetPortfolioTVLService = async (): Promise<VelvetTVLResponse>
 export const getVelvetPortfolioTransactions = async (
   portfolioAddress: Hash,
 ): Promise<AxiosResponse<VelvetPortfolioTransactionsResponse>> => {
-  return await axios.get(`https://intents.velvet.capital/api/v1/portfolio/transactions/${portfolioAddress}`)
+  return await axios.get(`${VELVET_CAPITAL_INTENTS_BASE_API_URL}/v1/portfolio/transactions/${portfolioAddress}`)
 }
